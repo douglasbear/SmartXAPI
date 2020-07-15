a9e44da4a899d1a584691059dc807499ddcc1f7c
sanjay-olivo <sanjay.kv@olivotech.com>
2020-07-15 12:27:56 +0530

    GL Account

--- Controllers/Accounts.cs
+++ Controllers/Accounts.cs
@@ -24,7 +24,7 @@ namespace SmartxAPI.Controllers
         }
 
         [HttpGet("glaccount/list")]
-        public ActionResult GetGLAccountList(int? nCompanyId,int? nFnYearId,int nPaymentMethodId)
+        public ActionResult GetGLAccountList(int? nCompanyId,int? nFnYearId,string xType)
         {
             if(nCompanyId==null){return StatusCode(404,_api.Response(404,"Company ID Required"));}                       
             if(nFnYearId==null){return StatusCode(404,_api.Response(404,"FnYear ID Required"));}                       
@@ -33,15 +33,8 @@ namespace SmartxAPI.Controllers
             SortedList Params=new SortedList();
             Params.Add("@p1",nCompanyId);
             Params.Add("@p2",nFnYearId);
-            Params.Add("@p3","A");
-            Params.Add("@p4","L");
-            string X_Crieteria="";
-             if (nPaymentMethodId == 2){X_Crieteria = "where N_CompanyID=@p1 and N_FnYearID=@p2 and (X_Type =@p3 or X_Type =@p4)";}
-            else{X_Crieteria = "where N_CompanyID=@p1 and N_FnYearID=@p2 and (X_Type =@p3 or X_Type =@p4) and (N_CashBahavID=@p5 or N_CashBahavID=@p6)";
-                Params.Add("@p5",4);
-                Params.Add("@p6",5);
-                }
-            string sqlCommandText="select [Account Code] as accountCode,Account,N_CompanyID,N_LedgerID,X_Level,N_FnYearID,N_CashBahavID,X_Type from vw_AccMastLedger "+X_Crieteria+" order by [Account Code]";
+            Params.Add("@p3",xType);
+            string sqlCommandText="select [Account Code] as accountCode,Account,N_CompanyID,N_LedgerID,X_Level,N_FnYearID,N_CashBahavID,X_Type from vw_AccMastLedger where N_CompanyID=@p1 and N_FnYearID=@p2 and X_Type =@p3  order by [Account Code]";
 
             try{
                 dt=dLayer.ExecuteDataTable(sqlCommandText,Params);
