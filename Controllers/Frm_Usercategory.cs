823a99264d482b755f95719e8f43f87933340119
ratheeshks-olivo <ratheeshkdj@gmail.com>
2020-07-21 12:45:29 +0530

    API Updates

--- Controllers/Frm_Usercategory.cs
+++ Controllers/Frm_Usercategory.cs
@@ -7,24 +7,31 @@ using System;
 using SmartxAPI.GeneralFunctions;
 using System.Data;
 using System.Collections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
 
 namespace SmartxAPI.Controllers
 {
     [Authorize(AuthenticationSchemes=JwtBearerDefaults.AuthenticationScheme)]
     [Route("usercategory")]
     [ApiController]
-    public class Acc_Usercategory : ControllerBase
+    public class Frm_Usercategory : ControllerBase
     {
         private readonly IApiFunctions _api;
         private readonly IDataAccessLayer dLayer;
+        private readonly string conString;
+        private readonly IMyFunctions myFunctions;
+        private readonly string connectionString;
         
-        public Acc_Usercategory(IApiFunctions api,IDataAccessLayer dl)
+        public Frm_Usercategory(IDataAccessLayer dl, IApiFunctions api, IMyFunctions myFun, IConfiguration conf)
         {
             _api=api;
             dLayer = dl;
+             myFunctions = myFun;
+            connectionString = conf.GetConnectionString("SmartxConnection");
         }
          [HttpGet("list")]
-        public ActionResult GetCategoryDetails(int? nCompanyId)
+        public ActionResult GetCategoryList(int? nCompanyId)
         {
             DataTable dt=new DataTable();
             SortedList Params=new SortedList();
@@ -33,13 +40,16 @@ namespace SmartxAPI.Controllers
             Params.Add("@p1",nCompanyId);
 
             try{
-                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params,connection);
+                    connection.Open();
+                }
+                dt = _api.Format(dt);
                      if(dt.Rows.Count==0)
                     {
-                       return StatusCode(200,new { StatusCode = 200 , Message= "No Results Found" });
-                    }else{
-                        return Ok(dt);
-                    }
+                       return StatusCode(200, _api.Response(200, "No Results Found")); }
+                       else{return Ok(dt);}
                 }catch(Exception e){
                     return StatusCode(403,_api.ErrorResponse(e));
                 }
@@ -58,16 +68,17 @@ namespace SmartxAPI.Controllers
             Params.Add("@p2",nCategoryId);
 
             try{
-                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params);
-                     if(dt.Rows.Count==0)
-                    {
-                       return StatusCode(200,new { StatusCode = 200 , Message= "No Results Found" });
-                    }else{
-                        return Ok(dt);
-                    }
-                }catch(Exception e){
-                    return StatusCode(403,_api.ErrorResponse(e));
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params,connection);
                 }
+                if(dt.Rows.Count==0)
+                {return StatusCode(200,new { StatusCode = 200 , Message= "No Results Found" });}
+                else{return Ok(dt);}
+                }
+            catch(Exception e){
+                    return StatusCode(403,_api.ErrorResponse(e));}
         }
 
 
@@ -76,10 +87,13 @@ namespace SmartxAPI.Controllers
         public ActionResult SaveData([FromBody]DataSet ds)
         { 
             try{
+
+                 using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
                     DataTable MasterTable;
                     MasterTable = ds.Tables["master"];
-                    dLayer.setTransaction();
-                   
                     SortedList Params = new SortedList();
                     // Auto Gen
                     string X_UserCategoryCode="";
@@ -89,7 +103,7 @@ namespace SmartxAPI.Controllers
                         Params.Add("N_YearID",MasterTable.Rows[0]["n_FnYearId"].ToString());
                         Params.Add("N_FormID",40);
                         Params.Add("N_BranchID",MasterTable.Rows[0]["n_BranchId"].ToString());
-                        X_UserCategoryCode =  dLayer.GetAutoNumber("sec_usercategory","X_UserCategoryCode", Params);
+                        X_UserCategoryCode =  dLayer.GetAutoNumber("sec_usercategory","X_UserCategoryCode", Params,connection,transaction);
                         if(X_UserCategoryCode==""){return StatusCode(409,_api.Response(409 ,"Unable to generate Category Code" ));}
                         MasterTable.Rows[0]["X_UserCategoryCode"] = X_UserCategoryCode;
                     }
@@ -97,18 +111,19 @@ namespace SmartxAPI.Controllers
                     MasterTable.Columns.Remove("n_FnYearId");
                     MasterTable.Columns.Remove("n_BranchId");
 
-                    int N_UserCategoryID=dLayer.SaveData("sec_usercategory","N_UserCategoryID",0,MasterTable);                    
+                    int N_UserCategoryID=dLayer.SaveData("sec_usercategory","N_UserCategoryID",0,MasterTable,connection,transaction);                    
                     if(N_UserCategoryID<=0){
-                        dLayer.rollBack();
+                        transaction.Rollback();
                         return StatusCode(404,_api.Response(404 ,"Unable to save" ));
                         }else{
-                    dLayer.commit();
+                    transaction.Commit();
                     return  GetCategoryDetails(int.Parse(MasterTable.Rows[0]["n_CompanyId"].ToString()),N_UserCategoryID);
                         }
                 }
+                }
                 catch (Exception ex)
                 {
-                    dLayer.rollBack();
+                   
                     return StatusCode(403,_api.ErrorResponse(ex));
                 }
         }
