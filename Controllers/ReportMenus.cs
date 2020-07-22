40f6473fdf3af13398bd214c7f5890cc5d9557b3
ratheeshks-olivo <ratheeshkdj@gmail.com>
2020-07-22 15:14:17 +0530

    API Updates

--- Controllers/ReportMenus.cs
+++ Controllers/ReportMenus.cs
@@ -12,7 +12,7 @@ using Microsoft.Data.SqlClient;
 
 namespace SmartxAPI.Controllers
 {
-    [Authorize(AuthenticationSchemes=JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("report")]
     [ApiController]
     public class ReportMenus : ControllerBase
@@ -22,95 +22,174 @@ namespace SmartxAPI.Controllers
         private readonly string conString;
         private readonly IMyFunctions myFunctions;
         private readonly string connectionString;
-        
+
         public ReportMenus(IDataAccessLayer dl, IApiFunctions api, IMyFunctions myFun, IConfiguration conf)
         {
-            _api=api;
+            _api = api;
             dLayer = dl;
-             myFunctions = myFun;
+            myFunctions = myFun;
             connectionString = conf.GetConnectionString("SmartxConnection");
         }
-         [HttpGet("list")]
-        public ActionResult GetReportList(int? nMenuId,int? nLangId)
+        [HttpGet("list")]
+        public ActionResult GetReportList(int? nMenuId, int? nLangId)
         {
-            DataTable dt=new DataTable();
-            SortedList Params=new SortedList();
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
 
-            string sqlCommandText="select * from vw_Menus where N_ParentMenuID=@p1 and N_LanguageId=@p2";
-            Params.Add("@p1",nMenuId);
-            Params.Add("@p2",nLangId);
+            string sqlCommandText = "select * from vw_Menus where N_ParentMenuID=@p1 and N_LanguageId=@p2";
+            Params.Add("@p1", nMenuId);
+            Params.Add("@p2", nLangId);
 
-            try{
+            try
+            {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params,connection);
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
                     DataTable dt1 = new DataTable();
-                    string sqlCommandText1="select N_MenuID,x_Text,x_FieldType,X_rptFile from vw_ReportMenus where N_LanguageId=@p2";
-                    dt1=dLayer.ExecuteDataTable(sqlCommandText1,Params,connection);
+
+
+                    string sqlCommandText1 = "select * from vw_ReportMenus where N_LanguageId=@p2";
+                    dt1 = dLayer.ExecuteDataTable(sqlCommandText1, Params, connection);
 
                     dt.Columns.Add("ChildMenus", typeof(DataTable));
                     dt.Columns.Add("Filter", typeof(DataTable));
-                    for(int i=0;i<dt.Rows.Count;i++)
+                    for (int i = 0; i < dt.Rows.Count; i++)
                     {
-                     DataTable ChildMenus = new DataTable();  
-                     DataTable Filter = new DataTable();  
-                     string N_MenuID =dt.Rows[i]["N_MenuID"].ToString();
-                     try{
-                     DataRow[] dr = dt1.Select("N_MenuID = " + N_MenuID +" and x_FieldType='RadioButton'"); 
-                     DataRow[] dr1 = dt1.Select("N_MenuID = " + N_MenuID +" and x_FieldType<>'RadioButton'"); 
-                     if(dr!=null)
-                     {ChildMenus = dr.CopyToDataTable();
-                     dt.Rows[i]["ChildMenus"]=ChildMenus;}
-                     if(dr1!=null){Filter = dr1.CopyToDataTable();
-                     dt.Rows[i]["Filter"]=Filter;}
-                     }
-                     catch
-                     {
-                     
-                     }
-                     
-                     
-                    
-                     
-                     }
-                  
+                        DataTable ChildMenus = new DataTable();
+                        DataTable Filter = new DataTable();
+                        string N_MenuID = dt.Rows[i]["N_MenuID"].ToString();
+                        try
+                        {
+                            DataRow[] dr = dt1.Select("N_MenuID = " + N_MenuID + " and x_FieldType='RadioButton'");
+                            DataRow[] dr1 = dt1.Select("N_MenuID = " + N_MenuID + " and x_FieldType<>'RadioButton'");
+                            if (dr != null)
+                            {
+                                ChildMenus = dr.CopyToDataTable();
+                                dt.Rows[i]["ChildMenus"] = ChildMenus;
+                            }
+                            if (dr1 != null)
+                            {
+                                Filter = dr1.CopyToDataTable();
+                                dt.Rows[i]["Filter"] = Filter;
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+
+
+
+                    }
+
 
                 }
 
-                     if(dt.Rows.Count==0)
-                    {
-                       return StatusCode(200, _api.Response(200, "No Results Found")); }
-                       else{return Ok(dt);}
-                }catch(Exception e){
-                    return StatusCode(403,_api.ErrorResponse(e));
+                if (dt.Rows.Count == 0)
+                {
+                    return StatusCode(200, _api.Response(200, "No Results Found"));
                 }
+                else { return Ok(dt); }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(403, _api.ErrorResponse(e));
+            }
         }
 
-       
 
-        [HttpGet("listdetails")]
-        public ActionResult GetCategoryDetails(int? nCompanyId,int? nCategoryId)
+        [HttpGet("dynamiclist")]
+        public ActionResult GetDynamicList(int nMenuId, int nCompId, int nLangId, string cval, string bval, string fval)
         {
-            DataTable dt=new DataTable();
-            SortedList Params=new SortedList();
-            
-            string sqlCommandText="select * from Sec_UserCategory where N_CompanyID=@p1 and N_UserCategoryID=@p2 order by N_UserCategoryID DESC";
-            Params.Add("@p1",nCompanyId);
-            Params.Add("@p2",nCategoryId);
+            DataTable dt = new DataTable();
+            DataTable outTable = new DataTable();
+            SortedList Params = new SortedList();
 
-            try{
+            string sqlCommandText = "select TOP 1 X_TableName,X_FieldList,X_Criteria from vw_ReportMenus where N_MenuID=@p1 and N_LanguageId=@p2 and N_CompID=@p3 and X_CompType=@p4";
+            Params.Add("@p1", nMenuId);
+            Params.Add("@p2", nLangId);
+            Params.Add("@p3", nCompId);
+            Params.Add("@p4", "ListControl");
+
+            try
+            {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    dt=dLayer.ExecuteDataTable(sqlCommandText,Params,connection);
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+                    if (dt.Rows.Count > 0)
+                    {
+                        DataRow QueryString = dt.Rows[0];
+                        SortedList ListSqlParams = new SortedList();
+                        string fields = QueryString["X_FieldList"].ToString();
+                        string table = QueryString["X_TableName"].ToString();
+                        string Criteria = QueryString["X_Criteria"].ToString().Replace("'CVal'", "@CVal").Replace("'BVal'", "@BVal").Replace("'FVal'", "@FVal");
+                        ListSqlParams.Add("@BVal", bval);
+                        ListSqlParams.Add("@CVal", cval);
+                        ListSqlParams.Add("@FVal", fval);
+                        string ListSql = "select " + fields + " from " + table + " where " + Criteria;
+
+                        outTable = dLayer.ExecuteDataTable(ListSql, ListSqlParams, connection);
+                    }
+
+
                 }
-                if(dt.Rows.Count==0)
-                {return StatusCode(200,new { StatusCode = 200 , Message= "No Results Found" });}
-                else{return Ok(dt);}
+                if (outTable.Rows.Count == 0)
+                {
+                    return StatusCode(200, _api.Response(200, "No Results Found"));
                 }
-            catch(Exception e){
-                    return StatusCode(403,_api.ErrorResponse(e));}
+                else {
+                    DataTable result=new DataTable();
+                    result.Columns.Add(nMenuId.ToString(), typeof(DataTable));
+                    DataTable compTable = new DataTable();  
+                    compTable.Columns.Add(nCompId.ToString(), typeof(DataTable));
+
+                    DataRow dRow1 = compTable.NewRow();
+                    compTable.Rows.InsertAt(dRow1, 0);
+                    compTable.Rows[0][nCompId.ToString()] = outTable;
+                    
+                    DataRow dRow2 = result.NewRow();
+                    result.Rows.InsertAt(dRow2, 0);
+                    result.Rows[0][nMenuId.ToString()]=compTable;
+
+                     return Ok(result); 
+                     }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(403, _api.ErrorResponse(e));
+            }
+        }
+
+
+
+        [HttpGet("listdetails")]
+        public ActionResult GetCategoryDetails(int? nCompanyId, int? nCategoryId)
+        {
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
+
+            string sqlCommandText = "select * from Sec_UserCategory where N_CompanyID=@p1 and N_UserCategoryID=@p2 order by N_UserCategoryID DESC";
+            Params.Add("@p1", nCompanyId);
+            Params.Add("@p2", nCategoryId);
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+                }
+                if (dt.Rows.Count == 0)
+                { return StatusCode(200, new { StatusCode = 200, Message = "No Results Found" }); }
+                else { return Ok(dt); }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(403, _api.ErrorResponse(e));
+            }
         }
 
 
