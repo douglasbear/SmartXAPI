06b347872b96903890c28a36dae23e29db6c1b8d
Sanjay <sanjay.kv@olivotech.com>
2020-10-31 13:53:55 +0530

    changes

--- Controllers/Ess_Dashboard.cs
+++ Controllers/Ess_Dashboard.cs
@@ -49,7 +49,7 @@ namespace SmartxAPI.Controllers
             string sqlCommandNextLeave = "SELECT CONVERT(VARCHAR,Pay_VacationDetails.D_VacDateFrom, 106) as D_VacDateFrom,CONVERT(VARCHAR, Pay_VacationDetails.D_VacDateTo, 106) as D_VacDateTo, Pay_VacationDetails.N_VacDays,( 60 * Pay_VacationDetails.N_VacDays) as N_hours,Pay_VacationType.X_VacType FROM  Pay_VacationDetails INNER JOIN Pay_VacationType ON Pay_VacationDetails.N_VacTypeID = Pay_VacationType.N_VacTypeID AND  Pay_VacationDetails.N_CompanyID = Pay_VacationType.N_CompanyID WHERE  (Pay_VacationDetails.N_CompanyID = @p1) AND (N_FnYearID = @p2) AND (N_EmpID = @p3) and   (Pay_VacationDetails.N_VacationID = (SELECT     MAX(N_VacationID) AS Expr1 FROM         Pay_VacationDetails AS Pay_VacationDetails_1 WHERE     (N_CompanyID = @p1) AND (N_FnYearID = @p2) AND (N_EmpID = @p3) and  n_vacdays<0 ))";
             string sqlCommandDailyLogin = "SELECT MAX(D_In) as D_In,Convert(Time, GetDate()) as D_Cur,cast(dateadd(millisecond, datediff(millisecond,MAX(D_In),Convert(Time, GetDate())), '19000101') AS TIME) AS duration from Pay_TimeSheetImport where N_EmpID=@p3 and D_Date=getdate()";
             string EnableLeave = "select N_Value from Gen_Settings where X_Group='EssOnline' and N_CompanyID=@p1";
-
+            string WorkerHours="select top(7) D_Date,N_EmpID,convert(varchar(5),DateDiff(s, D_In, D_Out)/3600)+':'+convert(varchar(5),DateDiff(s, D_In, D_Out)%3600/60)+':'+convert(varchar(5),(DateDiff(s, D_In, D_Out)%60)) as [hh:mm:ss] from Pay_TimeSheetImport where N_EmpID = @p3 order by D_Date desc";
 
 
             Params.Add("@p1", nCompanyID);
@@ -62,6 +62,7 @@ namespace SmartxAPI.Controllers
             DataTable LeaveDetails = new DataTable();
             DataTable NextLeaveDetails = new DataTable();
             DataTable DailyLogin = new DataTable();
+            DataTable WorkedHours = new DataTable();
 
             try
             {
@@ -120,6 +121,10 @@ namespace SmartxAPI.Controllers
                     DailyLogin = dLayer.ExecuteDataTable(sqlCommandDailyLogin, Params, connection);
                     DailyLogin = api.Format(DailyLogin, "DailyLogin");
 
+                    WorkedHours = dLayer.ExecuteDataTable(WorkerHours, Params, connection);
+                    WorkedHours = api.Format(WorkedHours, "Worked Hour");
+
+
                 }
                 dt.Tables.Add(EmployeeDetails);
                 DashboardDetails = api.Format(DashboardDetails, "DashboardDetails");
@@ -127,6 +132,7 @@ namespace SmartxAPI.Controllers
                 dt.Tables.Add(LeaveDetails);
                 dt.Tables.Add(NextLeaveDetails);
                 dt.Tables.Add(DailyLogin);
+                dt.Tables.Add(WorkedHours);
 
                 return Ok(api.Success(dt));
 
