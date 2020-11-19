ac2016fca9672c9f3a7601ea58b662ae01e48813
Sanjay <sanjay.kv@olivotech.com>
2020-11-19 13:21:44 +0530

    changes

--- Controllers/CRM_Activity.cs
+++ Controllers/CRM_Activity.cs
@@ -45,9 +45,9 @@ namespace SmartxAPI.Controllers
             string sqlCommandText ="";
              
              if(Count==0)
-                sqlCommandText = "select top("+ nSizeperpage +") * from vw_CRM_Activity where N_CompanyID=@p1 ";
+                sqlCommandText = "select top("+ nSizeperpage +") * from vw_CRM_Activity where N_CompanyID=@p1 order by n_activityid desc";
             else
-                sqlCommandText = "select top("+ nSizeperpage +") * from vw_CRM_Activity where N_CompanyID=@p1 and N_ActivityID not in (select top("+ Count +") N_ActivityID from vw_CRM_Activity where N_CompanyID=@p1)";
+                sqlCommandText = "select top("+ nSizeperpage +") * from vw_CRM_Activity where N_CompanyID=@p1 and N_ActivityID not in (select top("+ Count +") N_ActivityID from vw_CRM_Activity where N_CompanyID=@p1 order by n_activityid desc) order by n_activityid desc";
             Params.Add("@p1", nCompanyId);
 
             SortedList OutPut = new SortedList();
