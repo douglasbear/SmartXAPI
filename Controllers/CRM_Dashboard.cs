18f251e2045afc17e973c51f632fc5966992dfb0
Sanjay <sanjay.kv@olivotech.com>
2020-12-11 16:01:35 +0530

    changes

--- Controllers/CRM_Dashboard.cs
+++ Controllers/CRM_Dashboard.cs
@@ -31,39 +31,24 @@ namespace SmartxAPI.Controllers
             connectionString = conf.GetConnectionString("SmartxConnection");
         }
         [HttpGet("listDetails")]
-        public ActionResult GetCustomerDetails(int nEmpID, int nFnyearID)
+        public ActionResult GetDashboardDetails()
         {
             DataSet dt = new DataSet();
             SortedList Params = new SortedList();
             int nCompanyID = myFunctions.GetCompanyID(User);
             int nUserID = myFunctions.GetUserID(User);
 
-            string sqlCommandEmployeeDetails = "select CONVERT(VARCHAR,vw_PayEmployee.d_DOB, 106) as d_DOB1,CONVERT(VARCHAR,vw_PayEmployee.d_HireDate, 106) as d_HireDate1,* from vw_PayEmployee where N_CompanyID=@p1 and N_FnYearID=@p2 and N_EmpID=@p3";
-            // string sqlCommandLoan = "select SUM(N_LoanAmount) as N_LoanAmount from Pay_LoanIssue where N_CompanyID=@p1 and N_FnYearID=@p2 and N_EmpID=@p3 group by N_CompanyID,N_EmpID";
-            string sqlCommandLoan = "select ROUND(SUM(balance),2) from vw_LoanDetails where N_CompanyID =@p1 and N_EmpID =@p3";
-            string sqlCommandVacation = "Select SUM(N_VacDays) as N_VacDays from Pay_VacationDetails where N_CompanyID=@p1 and N_FnYearID=@p2 and N_EmpID=@p3 group by N_CompanyID,N_EmpID";
-            //string sqlCommandLeave = "SELECT Pay_VacationType.X_VacType,SUM(Pay_VacationDetails.N_VacDays) AS N_VacDays FROM  Pay_VacationDetails INNER JOIN Pay_EmpAccruls ON Pay_VacationDetails.N_CompanyID = Pay_EmpAccruls.N_CompanyID AND Pay_VacationDetails.N_EmpID = Pay_EmpAccruls.N_EmpId AND Pay_VacationDetails.N_VacTypeID = Pay_EmpAccruls.N_VacTypeID INNER JOIN Pay_VacationType ON Pay_EmpAccruls.N_VacTypeID = Pay_VacationType.N_VacTypeID AND Pay_EmpAccruls.N_CompanyID = Pay_VacationType.N_CompanyID WHERE     (Pay_VacationDetails.N_VacDays <> 0) and Pay_VacationDetails.N_CompanyID=@p1 and Pay_VacationDetails.N_FnYearID=@p2 and Pay_VacationDetails.N_EmpID=@p3 Group by Pay_VacationType.X_VacType";
-            string sqlCommandLeave = "Select N_VacTypeID ,N_Accrued ,N_MaxAvailDays, B_HolidayFlag ,X_VacType, 0 as X_Days ,X_Description, cast(N_Accrued as varchar)+case when N_Accrued =1 then ' Day/' else ' Days/'end + case when X_Period = 'M' then 'Month'  when X_Period ='Y' then 'Year' end as X_Accrued  from vw_pay_Vacation_List where X_Type='B' and N_EmpId = @p3 and N_CompanyID=@p1 and N_Accrued <>0 group by N_VacTypeID,X_VacType,N_Accrued,N_MaxAvailDays,B_HolidayFlag,X_Description,X_Period";
-            // string sqlCommandPendingVacation = "Select SUM(N_VacDays) from Pay_VacationDetails where N_VacDays < 0 and ISNULL(B_IsSaveDraft,0)<>0 and N_CompanyID=@p1 and N_FnYearID=@p2 and N_EmpID=@p3";
-            string sqlCommandPendingVacation = "select COUNT(*) AS N_LeaveRequest from vw_WebApprovalDashboard where N_NextApproverID =@p4 and N_CompanyID=@p1 and N_EmpID <> @p3 and N_VacationStatus not in (2,4)";
-            //string sqlCommandNextLeave = "SELECT CONVERT(VARCHAR,Pay_VacationDetails.D_VacDateFrom, 106) as D_VacDateFrom,CONVERT(VARCHAR, Pay_VacationDetails.D_VacDateTo, 106) as D_VacDateTo, Pay_VacationDetails.N_VacDays,( 24 * Pay_VacationDetails.N_VacDays) as N_hours,Pay_VacationType.X_VacType FROM  Pay_VacationDetails INNER JOIN Pay_VacationType ON Pay_VacationDetails.N_VacTypeID = Pay_VacationType.N_VacTypeID AND  Pay_VacationDetails.N_CompanyID = Pay_VacationType.N_CompanyID WHERE  (Pay_VacationDetails.N_CompanyID = @p1) AND (N_FnYearID = @p2) AND (N_EmpID = @p3) and   (Pay_VacationDetails.N_VacationID = (SELECT     MAX(N_VacationID) AS Expr1 FROM         Pay_VacationDetails AS Pay_VacationDetails_1 WHERE     (N_CompanyID = @p1) AND (N_FnYearID = @p2) AND (N_EmpID = @p3) and  n_vacdays<0 ))";
-            string sqlCommandNextLeave = "SELECT     CONVERT(VARCHAR, Pay_VacationDetails.D_VacDateFrom, 106) AS D_VacDateFrom, CONVERT(VARCHAR, Pay_VacationDetails.D_VacDateTo, 106) AS D_VacDateTo,Pay_VacationDetails.N_VacDays, 24 * Pay_VacationDetails.N_VacDays AS N_hours, Pay_VacationType.X_VacType FROM Pay_VacationDetails INNER JOIN Pay_VacationType ON Pay_VacationDetails.N_VacTypeID = Pay_VacationType.N_VacTypeID AND Pay_VacationDetails.N_CompanyID = Pay_VacationType.N_CompanyID INNER JOIN Pay_VacationMaster ON Pay_VacationDetails.N_VacationGroupID = Pay_VacationMaster.N_VacationGroupID AND Pay_VacationDetails.N_EmpID = Pay_VacationMaster.N_EmpID AND Pay_VacationDetails.N_CompanyID = Pay_VacationMaster.N_CompanyID WHERE (Pay_VacationDetails.N_CompanyID = @p1) and Pay_VacationMaster.B_IsSaveDraft=0 AND (Pay_VacationDetails.N_FnYearID = @p2) AND (Pay_VacationDetails.N_EmpID = @p3) AND (Pay_VacationDetails.N_VacationID =(SELECT     MAX(N_VacationID) AS Expr1 FROM Pay_VacationDetails AS Pay_VacationDetails_1 WHERE (N_CompanyID = @p1) AND (N_FnYearID = @p2) AND (N_EmpID = @p3) AND (N_VacDays < 0)))";
-            string sqlCommandDailyLogin = "SELECT MAX(D_In) as D_In,Convert(Time, GetDate()) as D_Cur,cast(dateadd(millisecond, datediff(millisecond,MAX(D_In),Convert(Time, GetDate())), '19000101') AS TIME) AS duration from Pay_TimeSheetImport where N_EmpID=@p3 and D_Date=getdate()";
-            string EnableLeave = "select N_Value from Gen_Settings where X_Group='EssOnline' and N_CompanyID=@p1";
-            string WorkerHours="select top(7) D_Date,N_EmpID,convert(varchar(5),DateDiff(s, D_In, D_Out)/3600)+':'+convert(varchar(5),DateDiff(s, D_In, D_Out)%3600/60)+':'+convert(varchar(5),(DateDiff(s, D_In, D_Out)%60)) as [hh:mm:ss] from Pay_TimeSheetImport where N_EmpID = @p3 order by D_Date desc";
-
-
-            Params.Add("@p1", nCompanyID);
-            Params.Add("@p2", nFnyearID);
-            Params.Add("@p3", nEmpID);
-            Params.Add("@p4", nUserID);
-
-            DataTable EmployeeDetails = new DataTable();
-            DataTable DashboardDetails = new DataTable();
-            DataTable LeaveDetails = new DataTable();
-            DataTable NextLeaveDetails = new DataTable();
-            DataTable DailyLogin = new DataTable();
-            DataTable WorkedHours = new DataTable();
+            string sqlCurrentLead = "SELECT COUNT(*) as N_Count FROM crm_leads WHERE MONTH(D_Entrydate) = MONTH(CURRENT_TIMESTAMP) AND YEAR(D_Entrydate) = YEAR(CURRENT_TIMESTAMP)";
+            string sqlCurrentCustomer = "SELECT COUNT(*) as N_Count FROM CRM_Customer WHERE MONTH(D_Entrydate) = MONTH(CURRENT_TIMESTAMP) AND YEAR(D_Entrydate) = YEAR(CURRENT_TIMESTAMP)";
+            string sqlPerformance = "SELECT 'Leads Created',COUNT(*) as N_Count FROM crm_leads WHERE D_Entrydate >= DATEADD(DAY, -90, GETDATE())union SELECT 'Opportunities Created',COUNT(*) as N_Count FROM CRM_Opportunity WHERE D_Entrydate >= DATEADD(DAY, -90, GETDATE()) union SELECT 'Customer Created',COUNT(*) as N_Count FROM CRM_Customer WHERE D_Entrydate >= DATEADD(DAY, -90, GETDATE()) union SELECT 'Contacts Created',COUNT(*) as N_Count FROM CRM_Contact WHERE D_Entrydate >= DATEADD(DAY, -90, GETDATE()) union SELECT 'Projects Created',COUNT(*) as N_Count FROM CRM_Project WHERE D_Entrydate >= DATEADD(DAY, -90, GETDATE())";
+            string sqlOpportunitiesStage = "select X_Stage,COUNT(*) as N_Count as N_Percentage  from vw_CRMOpportunity group by X_Stage";
+            string sqlLeadsbySource = "select X_LeadSource,COUNT(*) as N_Count as N_Percentage from vw_CRMLeads group by X_LeadSource";
+           
+            DataTable CurrentLead = new DataTable();
+            DataTable CurrentCustomer = new DataTable();
+            DataTable Performance = new DataTable();
+            DataTable OpportunitiesStage = new DataTable();
+            DataTable LeadsbySource = new DataTable();
 
             try
             {
@@ -71,57 +56,23 @@ namespace SmartxAPI.Controllers
                 {
                     connection.Open();
 
-                    EmployeeDetails = dLayer.ExecuteDataTable(sqlCommandEmployeeDetails, Params, connection);
-                    EmployeeDetails = api.Format(EmployeeDetails, "EmployeeDetails");
-                    EmployeeDetails = myFunctions.AddNewColumnToDataTable(EmployeeDetails, "EmployeeImage", typeof(string), null);
-                    if (EmployeeDetails.Rows[0]["i_Employe_Image"] != null)
-                    {
-                        DataRow dataRow = EmployeeDetails.Rows[0];
-                        string ImageData = dataRow["i_Employe_Image"].ToString();
-                        if (ImageData != "")
-                        {
-                            byte[] Image = (byte[])dataRow["i_Employe_Image"];
-                            EmployeeDetails.Rows[0]["EmployeeImage"] = "data:image/png;base64," + Convert.ToBase64String(Image, 0, Image.Length);
-                            EmployeeDetails.Columns.Remove("i_Employe_Image");
-                        }
-                        EmployeeDetails.AcceptChanges();
-                    }
-                    object EnableLeaveData = dLayer.ExecuteScalar(EnableLeave, Params, connection);
-                    object Loan = dLayer.ExecuteScalar(sqlCommandLoan, Params, connection);
-                    object TotalVacation=null;
-                    if(EnableLeaveData.ToString()=="1")
-                         TotalVacation = dLayer.ExecuteScalar(sqlCommandVacation, Params, connection);
-                    object PendingVacation = dLayer.ExecuteScalar(sqlCommandPendingVacation, Params, connection);
+                    CurrentLead = dLayer.ExecuteDataTable(sqlCurrentLead, Params, connection);
+                    CurrentLead = api.Format(CurrentLead, "CurrentLead");
+                    CurrentCustomer = dLayer.ExecuteDataTable(sqlCurrentCustomer, Params, connection);
+                    CurrentCustomer = api.Format(CurrentCustomer, "CurrentCustomer");
+                    Performance = dLayer.ExecuteDataTable(sqlPerformance, Params, connection);
+                    Performance = api.Format(Performance, "Performance");
+                    OpportunitiesStage = dLayer.ExecuteDataTable(sqlOpportunitiesStage, Params, connection);
+                    OpportunitiesStage = api.Format(OpportunitiesStage, "OpportunitiesStage");
+                    LeadsbySource = dLayer.ExecuteDataTable(sqlLeadsbySource, Params, connection);
+                    LeadsbySource = api.Format(LeadsbySource, "LeadsbySource");
                     
-
-                    DashboardDetails = myFunctions.AddNewColumnToDataTable(DashboardDetails, "Loan", typeof(string), Loan);
-                    DashboardDetails = myFunctions.AddNewColumnToDataTable(DashboardDetails, "Vacation", typeof(string), TotalVacation);
-                    DashboardDetails = myFunctions.AddNewColumnToDataTable(DashboardDetails, "PendingVacation", typeof(string), PendingVacation);
-                    DashboardDetails = myFunctions.AddNewColumnToDataTable(DashboardDetails, "EnableLeave", typeof(string), EnableLeaveData);
-                    DataRow row = DashboardDetails.NewRow();
-                    DashboardDetails.Rows.Add(row);
-                    DashboardDetails.AcceptChanges();
-                    LeaveDetails = api.Format(LeaveDetails, "EmployeeLeaves");
-
-
-                    NextLeaveDetails = dLayer.ExecuteDataTable(sqlCommandNextLeave, Params, connection);
-                    NextLeaveDetails = api.Format(NextLeaveDetails, "EmployeeNextLeave");
-
-                    DailyLogin = dLayer.ExecuteDataTable(sqlCommandDailyLogin, Params, connection);
-                    DailyLogin = api.Format(DailyLogin, "DailyLogin");
-
-                    WorkedHours = dLayer.ExecuteDataTable(WorkerHours, Params, connection);
-                    WorkedHours = api.Format(WorkedHours, "Worked Hour");
-
-
                 }
-                dt.Tables.Add(EmployeeDetails);
-                DashboardDetails = api.Format(DashboardDetails, "DashboardDetails");
-                dt.Tables.Add(DashboardDetails);
-                dt.Tables.Add(LeaveDetails);
-                dt.Tables.Add(NextLeaveDetails);
-                dt.Tables.Add(DailyLogin);
-                dt.Tables.Add(WorkedHours);
+                dt.Tables.Add(CurrentLead);
+                dt.Tables.Add(CurrentCustomer);
+                dt.Tables.Add(Performance);
+                dt.Tables.Add(OpportunitiesStage);
+                dt.Tables.Add(LeadsbySource);
 
                 return Ok(api.Success(dt));
 
