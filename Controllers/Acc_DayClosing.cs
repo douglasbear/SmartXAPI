c92e26ab8c76af6e359900f90c374f4fc23352dd
sibi <sibin.babu@olivotech.com>
2021-03-19 12:07:45 +0530

    API

--- Controllers/Acc_DayClosing.cs
+++ Controllers/Acc_DayClosing.cs
@@ -34,7 +34,7 @@ namespace SmartxAPI.Controllers
         }
 
         [HttpGet("salesSummary")]
-        public ActionResult GetSalesSummaryDetails(int nCompanyID,int nFnYearID,int nBranchID,DateTime dTransDate)
+        public ActionResult GetCashSummaryDetails(int nCompanyID,int nFnYearID,int nBranchID,DateTime dTransDate)
         {
             DataTable dt = new DataTable();
             SortedList Params = new SortedList();
@@ -43,6 +43,39 @@ namespace SmartxAPI.Controllers
             Params.Add("@p3",nBranchID);
             Params.Add("@p4",dTransDate);
             string sqlCommandText="Select N_CompanyID,SUM(N_AmountDr)as N_AmountDr,SUM(N_AmountCr) as N_AmountCr,D_TransDate,N_BranchID,N_FnYearID,X_CustomerName from  Vw_DailySalesSummary where N_CompanyID=@p1 and N_FnYearID=@p2 and N_BranchID=@p3 and D_TransDate=@p4 group by N_CompanyID,D_TransDate,N_BranchID,N_FnYearID,X_CustomerName";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params , connection);
+                }
+                dt = _api.Format(dt);
+                if (dt.Rows.Count == 0)
+                {
+                    return Ok(_api.Notice("No Results Found"));
+                }
+                else
+                {
+                    return Ok(_api.Success(dt));
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(_api.Error(e));
+            }
+        }
+
+        [HttpGet("cashSummary")]
+        public ActionResult GetSalesSummaryDetails(int nCompanyID,int nFnYearID,int nBranchID,DateTime dTransDate)
+        {
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
+            Params.Add("@p1",nCompanyID);
+            Params.Add("@p2",nFnYearID);
+            Params.Add("@p3",nBranchID);
+            Params.Add("@p4",dTransDate);
+            string sqlCommandText="Select * from  vw_VoucherTransaction where  and N_CompanyID=@p1 and N_FnYearID=@p2 and N_BranchID=@p3 and D_TransDate=@p4";
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
