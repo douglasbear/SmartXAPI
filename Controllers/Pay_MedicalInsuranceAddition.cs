57a620f9d5a336b669ea3bb4dfc5f1099ee04b3e
aswin <aswin.s@olivotech.com>
2021-05-05 15:04:09 +0530

    shift

--- Controllers/Pay_MedicalInsuranceAddition.cs
+++ Controllers/Pay_MedicalInsuranceAddition.cs
@@ -68,7 +68,7 @@ namespace SmartxAPI.Controllers
             DataTable dt = new DataTable();
             SortedList Params = new SortedList();
             int nCompanyID = myFunctions.GetCompanyID(User);
-            string sqlCommandText = "select * from vw_MedicalInsuranceAddition_Vendor where N_CompanyID=@nCompanyID ";
+            string sqlCommandText = "select * from vw_MedicalInsuranceAddition_Vendor  ";
             Params.Add("@nCompanyID", nCompanyID);
             try
             {
@@ -166,6 +166,130 @@ namespace SmartxAPI.Controllers
                 return Ok(_api.Error(e));
             }
         }
+
+            [HttpPost("save")]
+        public ActionResult SaveData([FromBody] DataSet ds)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    DataTable MasterTable;
+                    DataTable DetailTable;
+                    string DocNo = "";
+                    MasterTable = ds.Tables["master"];
+                    DetailTable = ds.Tables["details"];
+                    DataRow MasterRow = MasterTable.Rows[0];
+                    SortedList Params = new SortedList();
+                    int nCompanyID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_CompanyID"].ToString());
+                    int nAdditionID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_AdditionID"].ToString());
+                    int nFnYearID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_FnYearID"].ToString());
+                    string X_PolicyCode = MasterTable.Rows[0]["x_PolicyCode"].ToString();
+                    int nBranchID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_BranchID"].ToString());
+                    if (nAdditionID > 0)
+                    {
+                        dLayer.DeleteData("Pay_MedicalInsuranceAddition", "N_ReceiptId", nAdditionID, "N_CompanyID = " + nCompanyID, connection, transaction);
+                    }
+                    DocNo = MasterRow["x_PolicyCode"].ToString();
+                    if (X_PolicyCode == "@Auto")
+                    {
+                        Params.Add("N_CompanyID", nCompanyID);
+                        Params.Add("N_FormID", FormID);
+                        Params.Add("N_YearID", nFnYearID);
+                        while (true)
+                        {
+                            DocNo = dLayer.ExecuteScalarPro("SP_AutoNumberGenerate", Params, connection, transaction).ToString();
+                            object N_Result = dLayer.ExecuteScalar("Select 1 from Pay_MedicalInsuranceAddition Where X_PolicyCode ='" + DocNo + "' and N_CompanyID= " + nCompanyID, connection, transaction);
+                            if (N_Result == null)
+                                break;
+                        }
+                        X_PolicyCode = DocNo;
+                        if (X_PolicyCode == "") { transaction.Rollback(); return Ok(_api.Error("Unable to generate")); }
+                        MasterTable.Rows[0]["x_PolicyCode"] = X_PolicyCode;
+
+                    }
+                    string DupCriteria = "N_CompanyID=" + nCompanyID + " and N_FnYearID=" + nFnYearID + " and X_PolicyCode='" + X_PolicyCode + "'";
+                    string X_Criteria = "N_CompanyID=" + nCompanyID + " and N_FnYearID=" + nFnYearID;
+
+                    nAdditionID = dLayer.SaveData("Pay_MedicalInsuranceAddition", "N_AdditionID", DupCriteria, X_Criteria, MasterTable, connection, transaction);
+                    if (nAdditionID <= 0)
+                    {
+                        transaction.Rollback();
+                        return Ok(_api.Error("Unable To Save"));
+                    }
+                    int nAdditionDetailsID = dLayer.SaveData("Pay_MedicalInsuranceAdditionDetails", "N_AdditionDetailsID", DetailTable, connection, transaction);
+                    if (nAdditionDetailsID <= 0)
+                    {
+                        transaction.Rollback();
+                        return Ok(_api.Error("Unable To Save"));
+                    }
+                    transaction.Commit();
+                    return Ok(_api.Success("Saved"));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(_api.Error(ex));
+            }
+        }
+        [HttpGet("list")]
+        public ActionResult SalaryPayList(int nCompanyId, int nPage, int nSizeperpage, string xSearchkey, string xSortBy)
+        {
+            //int nCompanyId = myFunctions.GetCompanyID(User);
+            int nUserID = myFunctions.GetUserID(User);
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
+            string sqlCommandCount = "";
+            int Count = (nPage - 1) * nSizeperpage;
+            string sqlCommandText = "";
+            string Criteria = "";
+            string Searchkey = "";
+            if (xSearchkey != null && xSearchkey.Trim() != "")
+                Searchkey = "and Receipt No like '%" + xSearchkey + "%'";
+
+            if (xSortBy == null || xSortBy.Trim() == "")
+                xSortBy = " order by N_AdditionID asc";
+            else
+                xSortBy = " order by " + xSortBy;
+
+            if (Count == 0)
+                sqlCommandText = "select top(" + nSizeperpage + ") * from vw_MedicalInsuranceAdditionSearch where N_CompanyID=@nCompanyId " + Searchkey + Criteria + xSortBy;
+            else
+                sqlCommandText = "select top(" + nSizeperpage + ") * from vw_MedicalInsuranceAdditionSearch where N_CompanyID=@nCompanyId " + Searchkey + Criteria + " and N_AdditionID not in (select top(" + Count + ") N_AdditionID from vw_MedicalInsuranceAdditionSearch where N_CompanyID=@nCompanyId " + Criteria + xSortBy + " ) " + xSortBy;
+            Params.Add("@nCompanyId", nCompanyId);
+
+            SortedList OutPut = new SortedList();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+
+                    sqlCommandCount = "select count(*)x as N_Count  from vw_MedicalInsuranceAdditionSearch where N_CompanyID=@nCompanyId " + Searchkey + Criteria;
+                    object TotalCount = dLayer.ExecuteScalar(sqlCommandCount, Params, connection);
+                    OutPut.Add("Details", _api.Format(dt));
+                    OutPut.Add("TotalCount", TotalCount);
+                    if (dt.Rows.Count == 0)
+                    {
+                        return Ok(_api.Warning("No Results Found"));
+                    }
+                    else
+                    {
+                        return Ok(_api.Success(OutPut));
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Ok(_api.Error(e));
+            }
+        }
+
 
 
 
