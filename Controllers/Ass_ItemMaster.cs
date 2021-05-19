a5a341b90d4d62cccacec6536c077f20dbb4eb78
akshay-olivo <akshay.p@olivotech.com>
2021-05-19 11:58:02 +0530

    19 May

--- Controllers/Ass_ItemMaster.cs
+++ Controllers/Ass_ItemMaster.cs
@@ -99,96 +99,31 @@ namespace SmartxAPI.Controllers
             {
                 DataTable MasterTable;
                 MasterTable = ds.Tables["master"];
-                DataTable DetailsTable;
-                DetailsTable = ds.Tables["details"];
+                DataTable ExpiryTable;
+                ExpiryTable = ds.Tables["expiry"];
                 int nCompanyID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_CompanyId"].ToString());
-                int nAddlInfoID = myFunctions.getIntVAL(MasterTable.Rows[0]["N_AddlInfoID"].ToString());
-
-                int nCompanyID1 =myFunctions.getIntVAL(DetailsTable.Rows[0]["N_CompanyId"].ToString());
-                int nItemID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_ItemID"].ToString());
-                string dPlacedDate = DetailsTable.Rows[0]["D_PlacedDate"].ToString();
-                string xGISRefNo = DetailsTable.Rows[0]["X_GISRefNo"].ToString();
-                string xBarcode = DetailsTable.Rows[0]["X_Barcode"].ToString();
-                int nDepartmentID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_DepartmentID"].ToString());
-                int nEmpID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_EmpID"].ToString());
-                int nprojectID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_projectID"].ToString());
-                string xmake = DetailsTable.Rows[0]["X_make"].ToString();
-                string xModel = DetailsTable.Rows[0]["X_Model"].ToString(); 
-                string dWarExpDate = DetailsTable.Rows[0]["D_WarExpDate"].ToString(); 
-                string dLastMaintanance = DetailsTable.Rows[0]["D_LastMaintanance"].ToString(); 
-                string dNextMaintanance = DetailsTable.Rows[0]["D_NextMaintanance"].ToString(); 
-                string xDisposalRestrictions = DetailsTable.Rows[0]["X_DisposalRestrictions"].ToString(); 
-                string xLocation = DetailsTable.Rows[0]["X_Location"].ToString();
-                int nMaxMaintenance = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_MaxMaintenance"].ToString()); 
-                int nStatusID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_StatusID"].ToString());
-                string xNotes = DetailsTable.Rows[0]["X_Notes"].ToString();
-                string xPlateNumber = DetailsTable.Rows[0]["X_PlateNumber"].ToString();
-                string xSerialNo = DetailsTable.Rows[0]["X_SerialNo"].ToString();
-                string xContractNo = DetailsTable.Rows[0]["X_ContractNo"].ToString(); 
-                string xHypothecatedTo = DetailsTable.Rows[0]["X_HypothecatedTo"].ToString();
-                int nDwnPayemnt = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_DwnPayemnt"].ToString());
-                int nEmiAmount = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_EmiAmount"].ToString());
-                string dEmiDueDate = DetailsTable.Rows[0]["D_EmiDueDate"].ToString();
-                string dEmiStartDate = DetailsTable.Rows[0]["D_EmiStartDate"].ToString();
-                string dEmiEndDate = DetailsTable.Rows[0]["D_EmiEndDate"].ToString();
-                int nSalvageAmt = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_SalvageAmt"].ToString());
-                int nDeprCalcID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_DeprCalcID"].ToString());
-                int nDeprRate = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_DeprRate"].ToString());
-                int nCostCentreID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_CostCentreID"].ToString());
-                int nBranchID = myFunctions.getIntVAL(DetailsTable.Rows[0]["N_BranchID"].ToString());
-                
-
-
+                string xItemCode = MasterTable.Rows[0]["X_ItemCode"].ToString();
+                int nAddlInfoID = myFunctions.getIntVAL(ExpiryTable.Rows[0]["N_AddlInfoID"].ToString());
+                int nItemID = myFunctions.getIntVAL(MasterTable.Rows[0]["N_ItemID"].ToString());
                 
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
                     SqlTransaction transaction = connection.BeginTransaction();
-                    SortedList Params = new SortedList();
-                    Params.Add("N_CompanyID", nCompanyID1);
-
-                    Params.Add("N_ItemID", nItemID);
-                    Params.Add("D_PlacedDate", dPlacedDate);
-                    Params.Add("X_GISRefNo", xGISRefNo);
-                    Params.Add("X_Barcode", xBarcode);
-                    Params.Add("N_DepartmentID", nDepartmentID);
-                    Params.Add("N_EmpID", nEmpID);
-                    Params.Add("N_projectID", nprojectID);
-                    Params.Add("X_make", xmake);
-                    Params.Add("X_Model", xModel);
-                    Params.Add("D_WarExpDate", dWarExpDate);
-                    Params.Add("D_LastMaintanance", dLastMaintanance);
-                    Params.Add("D_NextMaintanance", dNextMaintanance);
-                    Params.Add("X_DisposalRestrictions", xDisposalRestrictions);
-                    Params.Add("X_Location",xLocation );
-                    Params.Add("N_MaxMaintenance", nMaxMaintenance);
-                    Params.Add("N_StatusID", nStatusID);
-                    Params.Add("X_Notes", xNotes);
-                    Params.Add("X_PlateNumber", xPlateNumber); 
-                    Params.Add("X_SerialNo", xSerialNo);
-                    Params.Add("X_ContractNo ", xContractNo );
-                    Params.Add("X_HypothecatedTo", xHypothecatedTo);
-                    Params.Add("N_DwnPayemnt", nDwnPayemnt);
-                    Params.Add("N_EmiAmount", nEmiAmount);
-                    Params.Add("D_EmiDueDate", dEmiDueDate);
-                    Params.Add("D_EmiStartDate",dEmiStartDate);
-                    Params.Add("D_EmiEndDate", dEmiEndDate);
-                    Params.Add("N_SalvageAmt", nSalvageAmt);
-                    Params.Add("N_DeprCalcID", nDeprCalcID);
-                    Params.Add("N_DeprRate", nDeprRate);
-                    Params.Add("N_CostCentreID", nCostCentreID);
-                    Params.Add("N_BranchID", nBranchID);
-                    
                    
-                     
-                    dLayer.ExecuteNonQuery(" Update Ass_AssetMaster Set D_PlacedDate=D_PlacedDate,X_GISRefNo=X_GISRefNo,X_Barcode=X_Barcode, N_DepartmentID=N_DepartmentID,N_EmpID=N_EmpID, N_ProjectID=N_ProjectID, X_make=X_make,X_Model=X_Model,D_WarExpDate=D_WarExpDate,D_LastMaintanance=D_LastMaintanance ,D_NextMaintanance=D_NextMaintanance, X_DisposalRestrictions=X_DisposalRestrictions,X_Location=X_Location,N_MaxMaintenance=N_MaxMaintenance,N_StatusID=N_StatusID,X_Notes=X_Notes,X_PlateNumber=X_PlateNumber,X_SerialNo = X_SerialNo,X_ContractNo =X_ContractNo,X_HypothecatedTo=X_HypothecatedTo,N_DwnPayemnt=N_DwnPayemnt,N_EmiAmount=N_EmiAmount,D_EmiDueDate=D_EmiDueDate,D_EmiStartDate=D_EmiStartDate,D_EmiEndDate=D_EmiEndDate,N_SalvageAmt=N_SalvageAmt,N_DeprCalcID=N_DeprCalcID ,N_DeprRate=N_DeprRate,N_CostCentreID=N_CostCentreID,N_BranchID=N_BranchID Where N_ItemID=N_ItemID and N_CompanyID=N_CompanyID",Params, connection,transaction);
-                    
+                    String DupCriteria ="X_ItemCode= '" + xItemCode + "' and N_CompanyID=" + nCompanyID;
+                    nItemID = dLayer.SaveData("Ass_AssetMaster", "N_ItemID", DupCriteria, "", MasterTable, connection, transaction);
+                    if(nItemID<=0)
+                    {
+                        transaction.Rollback();
+                        return Ok(api.Error("Unable to save"));
+                    }
                     if(nItemID>0)
                     {
                        dLayer.DeleteData("Ass_AssetAddlInfo", "N_ItemID", nItemID, "N_CompanyID=" + nCompanyID , connection,transaction);
                     }
 
-                    nAddlInfoID = dLayer.SaveData("Ass_AssetAddlInfo", "n_AddlInfoID", MasterTable, connection, transaction);
+                    nAddlInfoID = dLayer.SaveData("Ass_AssetAddlInfo", "n_AddlInfoID", ExpiryTable, connection, transaction);
                    
                     if (nAddlInfoID <= 0)
                     {
