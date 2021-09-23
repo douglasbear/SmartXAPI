cefca5fe2e8b40d40070c8f4451f77f1525573a1
aswin <aswin.s@olivotech.com>
2021-09-23 09:59:48 +0530

    scofield

--- Controllers/Inv_Maintanance.cs
+++ Controllers/Inv_Maintanance.cs
@@ -191,6 +191,8 @@ namespace SmartxAPI.Controllers
                         Mastersql = "select * from Vw_WarrantyToMaintananceMaster where N_CompanyId=@nCompanyID and N_WarrantyID=" + nWarrantyID + "  ";
                         MasterTable = dLayer.ExecuteDataTable(Mastersql, Params, connection);
                         if (MasterTable.Rows.Count == 0) { return Ok(_api.Warning("No data found")); }
+                         MasterTable.AcceptChanges();
+                        MasterTable = _api.Format(MasterTable, "Master");
 
 
                         DetailSql = "select * from Vw_WarrantyToMaintananceDetails where N_CompanyId=@nCompanyID and  N_WarrantyID=" + nWarrantyID + " ";
@@ -198,7 +200,7 @@ namespace SmartxAPI.Controllers
                         DetailTable = _api.Format(DetailTable, "Details");
 
 
-                        MasterTable.AcceptChanges();
+                       
                         dt.Tables.Add(MasterTable);
                         dt.Tables.Add(DetailTable);
                         return Ok(_api.Success(dt));
