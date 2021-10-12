a05f8c172d5dbc3c4ee386c902fc55801e203271
abhiragk <45223750+abhiragk@users.noreply.github.com>
2021-10-12 15:19:13 +0530

    updates

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -124,6 +124,6 @@ namespace SmartxAPI
         public static string _FontName = "Verdana";
         public static string prevVal = "";
         public static string newVal = "";
-        public static string _APIVersion = "21.9.30.1";
+        public static string _APIVersion = "21.10.9.1";
     }
 }
