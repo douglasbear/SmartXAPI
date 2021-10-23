59492de3edcd60be36c1210e635c904b1dd5effb
sibi <sibin.babu@olivotech.com>
2021-10-23 10:08:28 +0530

    API Oct 23

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -125,6 +125,6 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
         // public static string _APIVersion = "21.10.4.3"; //Provision
-        public static string _APIVersion = "21.10.22.1";
+        public static string _APIVersion = "21.10.9.1";
     }
 } 
