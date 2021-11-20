57bcb15b3996ccc19eca306ecd54ddc9dd8e7388
amegha-olivo <amegha.pp@olivotech.com>
2021-11-20 13:00:53 +0530

    updates

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -125,7 +125,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-       public static string _APIVersion = "21.11.16.1";
+       public static string _APIVersion = "21.10.9.1";
 
     }
 } 
