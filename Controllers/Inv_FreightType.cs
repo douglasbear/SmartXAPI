0a22c2ed639717895e3718cd75e00157296348b1
sibi <sibin.babu@olivotech.com>
2021-11-25 12:57:56 +0530

    Nov 25

---
+++ Controllers/Inv_FreightType.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using SmartxAPI.GeneralFunctions;
+using System.Data;
+using System.Collections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+namespace SmartxAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("freightType")]
+    [ApiController]
+    public class Inv_FreightType : ControllerBase
+    {
+        private readonly IApiFunctions _api;
+        private readonly IDataAccessLayer dLayer;
+        private readonly int FormID;
+        private readonly IMyFunctions myFunctions;
+        private readonly IMyAttachments myAttachments;
+        private readonly string connectionString;
+
+        public Inv_FreightType(IApiFunctions api, IDataAccessLayer dl, IMyFunctions myFun, IConfiguration conf, IMyAttachments myAtt)
+        {
+            _api = api;
+            dLayer = dl;
+            myFunctions = myFun;
+            myAttachments = myAtt;
+            connectionString = conf.GetConnectionString("SmartxConnection");
+            FormID = 395;
+        }
+
+        [HttpGet("list")]
+        public ActionResult FreightTypeList()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    DataTable dt = new DataTable();
+                    SortedList Params = new SortedList();
+                    int nCompanyID = myFunctions.GetCompanyID(User);
+                    Params.Add("@p1", nCompanyID);
+
+                    string sqlCommandText = "select * from Inv_PurchaseFreightReason where N_CompanyID=@p1 order by N_ReasonID asc";
+
+                    SortedList OutPut = new SortedList();
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+                    dt = _api.Format(dt);
+                    if (dt.Rows.Count == 0)
+                    {
+                        return Ok(_api.Warning("No Results Found"));
+                    }
+                    else
+                    {
+                        return Ok(_api.Success(dt));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_api.Error(User, e));
+            }
+        }
+
+    }
+}
+        
