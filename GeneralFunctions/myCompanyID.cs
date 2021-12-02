c61dd46dce7501e17442e6e1e9271751e16d77df
sanjay-olivo <sanjay.kv@olivotech.com>
2021-12-02 11:29:06 +0530

    changes

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -130,8 +130,8 @@ namespace SmartxAPI
         // public static string _APIVersion = "21.11.8.2"; 
         //  public static string _APIVersion = "21.11.29.2"; 
         // public static string _APIVersion = "21.11.10.1"; 
-        //  public static string _APIVersion = "21.10.9.1";
-       public static string _APIVersion = "21.12.1.2";
+          public static string _APIVersion = "21.10.9.1";
+      // public static string _APIVersion = "21.12.1.2";
 
     }
 } 
