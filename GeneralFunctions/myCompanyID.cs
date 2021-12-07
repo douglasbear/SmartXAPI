652567c8a9f46b2528e84546b5b86befb1e7f584
aswin <aswin.s@olivotech.com>
2021-12-07 12:01:13 +0530

    scofield

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -128,7 +128,7 @@ namespace SmartxAPI
        //public static string _APIVersion = "21.11.13.1";
 
         // public static string _APIVersion = "21.11.8.2"; 
-         public static string _APIVersion = "21.10.8.2"; 
+         public static string _APIVersion = "21.10.9.1"; 
         // public static string _APIVersion = "21.11.10.1"; 
         //  public static string _APIVersion = "21.10.9.1";
     //   public static string _APIVersion = "21.12.1.2";
