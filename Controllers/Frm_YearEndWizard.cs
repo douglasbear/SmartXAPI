2ba3d28b718f3e029758a10bade7619766f56d9d
sibi <sibin.babu@olivotech.com>
2021-12-09 18:59:04 +0530

    Dec 09

---
+++ Controllers/Frm_YearEndWizard.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using SmartxAPI.GeneralFunctions;
+using System.Data;
+using System.Collections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+namespace SmartxAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("yearEndWizard")]
+    [ApiController]
+    public class Frm_YearEndWizard : ControllerBase
+    {
+        private readonly IApiFunctions _api;
+        private readonly IDataAccessLayer dLayer;
+        private readonly int FormID;
+        private readonly IMyFunctions myFunctions;
+        private readonly IMyAttachments myAttachments;
+        private readonly string connectionString;
+
+        public Frm_YearEndWizard(IApiFunctions api, IDataAccessLayer dl, IMyFunctions myFun, IConfiguration conf, IMyAttachments myAtt)
+        {
+            _api = api;
+            dLayer = dl;
+            myFunctions = myFun;
+            myAttachments = myAtt;
+            connectionString = conf.GetConnectionString("SmartxConnection");
+            //FormID = 395;
+        }
+
+        [HttpGet("loadNextYear")]
+        public ActionResult LoadNextYear()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    DataTable dt = new DataTable();
+                    SortedList Params = new SortedList();
+                    int nCompanyID = myFunctions.GetCompanyID(User);
+                    Params.Add("N_CompanyID", nCompanyID);
+
+                    SortedList OutPut = new SortedList();
+                    dt = dLayer.ExecuteDataTablePro("SP_Acc_NextYear_Sel", Params, connection, transaction);
+                    dt = _api.Format(dt);
+                    if (dt.Rows.Count == 0)
+                    {
+                        return Ok(_api.Warning("No Results Found"));
+                    }
+                    else
+                    {
+                        return Ok(_api.Success(dt));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(_api.Error(User,e));
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
