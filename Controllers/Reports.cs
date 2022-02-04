3b3868a76fcadacbeec5008ed76e5efa3f8f4ed3
sanjay-olivo <sanjay.kv@olivotech.com>
2022-02-04 11:38:00 -0800

    changes

--- Controllers/Reports.cs
+++ Controllers/Reports.cs
@@ -411,9 +411,19 @@ namespace SmartxAPI.Controllers
                             critiria = critiria + " and {" + TableName + ".N_CompanyID}=" + myFunctions.GetCompanyID(User);
                         }
                         ReportName = ReportName.Replace("&", "");
+                        if(nFormID==894)
+                        {
+                            partyName="Inv";
+                            docNumber="1";
+                        }
                         partyName = partyName.Replace("&", "");
                         partyName = partyName.ToString().Substring(0, Math.Min(12, partyName.ToString().Length));
                         Regex.Replace(docNumber, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
+                        // docNumber.Replace(@"\", "");
+                        // docNumber.Replace("//", "");
+                        if(docNumber.Contains("/"))
+                            docNumber = docNumber.ToString().Substring(0, Math.Min(3, docNumber.ToString().Length));
+
                         string URL = reportApi + "api/report?reportName=" + ReportName + "&critiria=" + critiria + "&path=" + this.TempFilesPath + "&reportLocation=" + RPTLocation + "&dbval=" + dbName + "&random=" + random + "&x_comments=&x_Reporttitle=&extention=pdf&N_FormID=" + nFormID + "&QRUrl=" + QRurl + "&N_PkeyID=" + nPkeyID + "&partyName=" + partyName + "&docNumber=" + docNumber + "&formName=" + FormName;
                         var path = client.GetAsync(URL);
                         if (nFormID == 80)
