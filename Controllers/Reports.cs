3aa3875f2a18313669ec5d8f6359dc5baf8e61b0
sanjay-olivo <sanjay.kv@olivotech.com>
2022-02-07 09:55:13 -0800

    changes

--- Controllers/Reports.cs
+++ Controllers/Reports.cs
@@ -418,9 +418,8 @@ namespace SmartxAPI.Controllers
                         }
                         partyName = partyName.Replace("&", "");
                         partyName = partyName.ToString().Substring(0, Math.Min(12, partyName.ToString().Length));
-                        Regex.Replace(docNumber, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
-                        // docNumber.Replace(@"\", "");
-                        // docNumber.Replace("//", "");
+                        docNumber=Regex.Replace(docNumber, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
+                        partyName=Regex.Replace(partyName, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
                         if(docNumber.Contains("/"))
                             docNumber = docNumber.ToString().Substring(0, Math.Min(3, docNumber.ToString().Length));
 
