3437d0cb235d39a4e5aea8d94b1830c942a2c490
sanjay-olivo <sanjay.kv@olivotech.com>
2022-02-07 20:07:14 -0800

    chnages

--- Controllers/Reports.cs
+++ Controllers/Reports.cs
@@ -411,18 +411,18 @@ namespace SmartxAPI.Controllers
                             critiria = critiria + " and {" + TableName + ".N_CompanyID}=" + myFunctions.GetCompanyID(User);
                         }
                         ReportName = ReportName.Replace("&", "");
-                        if(nFormID==894)
+                        if (nFormID == 894)
                         {
-                            partyName="Inv";
-                            docNumber="1";
+                            partyName = "Inv";
+                            docNumber = "1";
                         }
+                        if (partyName == "" || partyName==null)
+                            partyName = "customer";
                         partyName = partyName.Replace("&", "");
                         partyName = partyName.ToString().Substring(0, Math.Min(12, partyName.ToString().Length));
-                        docNumber=Regex.Replace(docNumber, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
-                        partyName=Regex.Replace(partyName, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
-                        if(partyName=="")
-                        partyName="invoice";
-                        if(docNumber.Contains("/"))
+                        docNumber = Regex.Replace(docNumber, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
+                        partyName = Regex.Replace(partyName, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
+                        if (docNumber.Contains("/"))
                             docNumber = docNumber.ToString().Substring(0, Math.Min(3, docNumber.ToString().Length));
 
                         string URL = reportApi + "api/report?reportName=" + ReportName + "&critiria=" + critiria + "&path=" + this.TempFilesPath + "&reportLocation=" + RPTLocation + "&dbval=" + dbName + "&random=" + random + "&x_comments=&x_Reporttitle=&extention=pdf&N_FormID=" + nFormID + "&QRUrl=" + QRurl + "&N_PkeyID=" + nPkeyID + "&partyName=" + partyName + "&docNumber=" + docNumber + "&formName=" + FormName;
