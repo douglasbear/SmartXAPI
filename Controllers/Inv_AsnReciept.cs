6fedca9a60acf46a77c96422342087fd9b9dc1d7
amegha-olivo <amegha.pp@olivotech.com>
2022-03-10 09:27:28 +0530

    updates

--- Controllers/Inv_AsnReciept.cs
+++ Controllers/Inv_AsnReciept.cs
@@ -32,6 +32,91 @@ namespace SmartxAPI.Controllers
 
         }
 
+
+         [HttpGet("list")]
+        public ActionResult GetAsnReceipt(int nComapanyId, int nFnYearId,int nBranchID, int nPage, int nSizeperpage, string xSearchkey, string xSortBy,bool bAllBranchData)
+        {
+            
+            int nCompanyId = myFunctions.GetCompanyID(User);
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
+
+            int Count = (nPage - 1) * nSizeperpage;
+            string sqlCommandText = "";
+            string sqlCommandCount = "";
+            string sqlCondition = "";
+             string Searchkey = "";
+
+            if (xSearchkey != null && xSearchkey.Trim() != "")
+                Searchkey = "and (X_AsnDocNo like '%" + xSearchkey + "%' OR N_AsnID like '%" + xSearchkey + "%')";
+
+             if (xSortBy == null || xSortBy.Trim() == "")
+                xSortBy = " order by N_AsnID desc";
+            else
+            {
+                switch (xSortBy.Split(" ")[0])
+                {
+                    case "X_AsnDocNo":
+                        xSortBy = "X_AsnDocNo " + xSortBy.Split(" ")[1];
+                        break;
+                    case "N_AsnID":
+                        xSortBy = "N_AsnID " + xSortBy.Split(" ")[1];
+                        break;
+                    default: break;
+                }
+                xSortBy = " order by " + xSortBy;
+            }
+
+            Params.Add("@nCompanyId", nCompanyId);
+            Params.Add("@nFnYearId", nFnYearId);
+            Params.Add("@FormID", FormID);
+             Params.Add("@nBranchID", nBranchID);
+        
+            SortedList OutPut = new SortedList();
+    
+
+           
+         try
+            {
+              using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    if (bAllBranchData)
+                        sqlCondition = "N_CompanyID=@nCompanyId and N_FnYearID=@nFnYearId";
+                    else
+                        sqlCondition = "N_CompanyID=@nCompanyId and N_FnYearID=@nFnYearId and N_BranchID=@nBranchID";
+
+
+                    if (Count == 0)
+                        sqlCommandText = "select  top(" + nSizeperpage + ") * from vw_AsnReceipt where " + sqlCondition + " " + Searchkey + " " + xSortBy;
+                    else
+                        sqlCommandText = "select  top(" + nSizeperpage + ") * from vw_AsnReceipt where " + sqlCondition + " " + Searchkey + " and N_AsnID not in (select top(" + Count + ") N_AsnID from vw_AsnReceipt where " + sqlCondition + " " + xSortBy + " ) " + xSortBy;
+
+
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+                    sqlCommandCount = "select count(*) as N_Count from vw_AsnReceipt where " + sqlCondition + " " + Searchkey + "";
+                    object TotalCount = dLayer.ExecuteScalar(sqlCommandCount, Params, connection);
+                    OutPut.Add("Details", _api.Format(dt));
+                    OutPut.Add("TotalCount", TotalCount);
+                    if (dt.Rows.Count == 0)
+                    {
+                        return Ok(_api.Warning("No Results Found"));
+                    }
+                    else
+                    {
+                        return Ok(_api.Success(OutPut));
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(_api.Error(User, e));
+            }
+        }
+
+
         [HttpPost("save")]
        
           public ActionResult SaveData([FromBody] DataSet ds)
@@ -71,6 +156,12 @@ namespace SmartxAPI.Controllers
                         MasterTable.Rows[0]["X_AsnDocNo"] = X_AsnDocNo;
                     }
 
+                       if (nAsnID > 0)
+                    {
+                        dLayer.DeleteData("Wh_AsnDetails", "N_AsnID", nAsnID, "N_CompanyID=" + nCompanyID + " and N_AsnID=" + nAsnID, connection, transaction);
+                        dLayer.DeleteData("Wh_AsnMaster", "N_AsnID", nAsnID, "N_CompanyID=" + nCompanyID + " and N_AsnID=" + nAsnID, connection, transaction);
+                    }
+
                      nAsnID = dLayer.SaveData("Wh_AsnMaster", "N_AsnID", MasterTable, connection, transaction);
 
                       if (nAsnID <= 0)
@@ -81,7 +172,7 @@ namespace SmartxAPI.Controllers
 
                      for (int j = 0; j < DetailTable.Rows.Count; j++)
                     {
-                        DetailTable.Rows[j]["N_AsnDetailsID"] = nAsnID;
+                        DetailTable.Rows[j]["N_AsnID"] = nAsnID;
                     }
 
                      int N_AsnDetailsID = dLayer.SaveData("Wh_AsnDetails", "N_AsnDetailsID", DetailTable, connection, transaction);
@@ -104,6 +195,80 @@ namespace SmartxAPI.Controllers
                 return Ok(_api.Error(User,ex));
             }
           }
+
+       
+          [HttpGet("details")]
+        public ActionResult GetDetails(string xAsnDocNo, int nFnYearID, int nBranchID, bool bShowAllBranchData)
+        {
+            DataTable Master = new DataTable();
+            DataTable Detail = new DataTable();
+            DataSet ds = new DataSet();
+            SortedList Params = new SortedList();
+            SortedList QueryParams = new SortedList();
+
+            int companyid = myFunctions.GetCompanyID(User);
+
+            QueryParams.Add("@nCompanyID", companyid);
+            QueryParams.Add("@xAsnDocNo", xAsnDocNo);
+            QueryParams.Add("@nBranchID", nBranchID);
+            QueryParams.Add("@nFnYearID", nFnYearID);
+            string Condition = "";
+            string _sqlQuery = "";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    if (bShowAllBranchData == true)
+                        Condition = "n_Companyid=@nCompanyID and X_AsnDocNo =@xAsnDocNo ";
+                    else
+                        Condition = "n_Companyid=@nCompanyID and X_AsnDocNo =@xAsnDocNo and N_BranchID=@nBranchID";
+
+
+                    _sqlQuery = "Select * from vw_Wh_AsnMaster_Disp Where " + Condition + "";
+
+                    Master = dLayer.ExecuteDataTable(_sqlQuery, QueryParams, connection);
+
+                    Master = _api.Format(Master, "master");
+
+                    if (Master.Rows.Count == 0)
+                    {
+                        return Ok(_api.Notice("No Results Found"));
+                    }
+                    else
+                    {
+                         QueryParams.Add("@N_AsnID", Master.Rows[0]["N_AsnID"].ToString());
+
+                        // QueryParams.Add("@N_AsnDetailsID",N_AsnDetailsID);
+
+
+                        ds.Tables.Add(Master);
+        
+
+                        _sqlQuery = "Select * from vw_Wh_AsnDetails_Disp Where N_CompanyID=@nCompanyID and N_AsnID=@N_AsnID";
+                        Detail = dLayer.ExecuteDataTable(_sqlQuery, QueryParams, connection);
+
+                        Detail = _api.Format(Detail, "details");
+                        if (Detail.Rows.Count == 0)
+                        {
+                            return Ok(_api.Notice("No Results Found"));
+                        }
+                        ds.Tables.Add(Detail);
+
+                        return Ok(_api.Success(ds));
+                    }
+
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                return Ok(_api.Error(User,e));
+            }
+        }
 
 
 
