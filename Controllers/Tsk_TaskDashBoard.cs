26336fe5fd0d514dc06fc98f594d71676a18208b
sanjay-olivo <sanjay.kv@olivotech.com>
2022-03-26 15:39:19 +0530

    changes

--- Controllers/Tsk_TaskDashBoard.cs
+++ Controllers/Tsk_TaskDashBoard.cs
@@ -30,7 +30,8 @@ namespace SmartxAPI.Controllers
             myFunctions = myFun;
             connectionString = conf.GetConnectionString("SmartxConnection");
         }
-        [HttpGet("details")]
+     
+ [HttpGet("details")]
         public ActionResult GetDashboardDetails(int nFnYearId, int nUserID, int N_Year)
         {
             SortedList Params = new SortedList();
@@ -39,11 +40,11 @@ namespace SmartxAPI.Controllers
             string UserPattern = myFunctions.GetUserPattern(User);
             string Pattern = "";
             string AssigneePattern = "";
-            string sqlActiveEmployees = "SELECT COUNT(*) as N_ActiveEmloyees FROM vw_CRMLeads WHERE N_CompanyID=" + nCompanyID + " and N_Status not in (2,3)";//Active employees
-            string sqlScheduledList = "select  COUNT(*) as N_ToDoList from  vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_AssigneeID=" + nUserID + " and and isnull(B_Closed,0) =0";
+            string sqlActiveEmployees = "SELECT COUNT(*) as N_ActiveEmloyees FROM pay_employee WHERE N_CompanyID=" + nCompanyID + " and N_Status not in (2,3)";//Active employees
+            string sqlScheduledList = "select  COUNT(*) as N_ToDoList from  vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_AssigneeID=" + nUserID + " and isnull(B_Closed,0) =0";
             string sqlActiveList = "select Count(*) as N_ActiveList from vw_Tsk_TaskCurrentStatus where N_Status=7 and N_CompanyID=" + nCompanyID + " and  N_CreaterID=" + nUserID + "";
-            string sqlCompletedList = "select Count(*) from N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=4";
-            string sqlClosedList = "select Count(*) from N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=5";
+            string sqlCompletedList = "select Count(*) as N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=4";
+            string sqlClosedList = "select Count(*) as N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=5";
             string sqlTaskStatus = "";
             SortedList Data = new SortedList();
             DataTable ActiveEmployees = new DataTable();
@@ -54,34 +55,19 @@ namespace SmartxAPI.Controllers
             DataTable TaskStatus = new DataTable();
 
 
-            object ActiveEmp = "";
-            object ScheduledTask = "";
-            object ActiveTask = "";
-            object CompletedTask = "";
-            object ClosedTask = "";
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
               
                 {
                     connection.Open();
-                    ActiveEmp = dLayer.ExecuteDataTable(sqlActiveEmployees, Params, connection);
-                    ScheduledTask = dLayer.ExecuteDataTable(sqlScheduledList, Params, connection);
-                    ActiveTask = dLayer.ExecuteDataTable(sqlActiveList, Params, connection);
-                    CompletedTask = dLayer.ExecuteDataTable(sqlCompletedList, Params, connection);
-                    ClosedTask = dLayer.ExecuteDataTable(sqlClosedList, Params, connection);
+                    ActiveEmployees = dLayer.ExecuteDataTable(sqlActiveEmployees, Params, connection);
+                    ScheduledList = dLayer.ExecuteDataTable(sqlScheduledList, Params, connection);
+                    ActiveList = dLayer.ExecuteDataTable(sqlActiveList, Params, connection);
+                    CompletedList = dLayer.ExecuteDataTable(sqlCompletedList, Params, connection);
+                    ClosedList = dLayer.ExecuteDataTable(sqlClosedList, Params, connection);
 
-                    ActiveEmployees = myFunctions.AddNewColumnToDataTable(ActiveEmployees, "N_ActiveEmloyees", typeof(string), ActiveEmp);
-                    ActiveEmployees.AcceptChanges();
-                    ScheduledList = myFunctions.AddNewColumnToDataTable(ScheduledList, "N_ScheduledTask", typeof(string), ScheduledTask);
-                    ScheduledList.AcceptChanges();
-                    ActiveList = myFunctions.AddNewColumnToDataTable(ActiveList, "N_ActiveTask", typeof(string), ActiveTask);
-                    ActiveList.AcceptChanges();
-                    CompletedList = myFunctions.AddNewColumnToDataTable(CompletedList, "N_CompletedTask", typeof(string), CompletedTask);
-                    CompletedList.AcceptChanges();
-                    ClosedList = myFunctions.AddNewColumnToDataTable(ClosedList, "N_ClosedTask", typeof(string), ClosedTask);
-                    ClosedList.AcceptChanges();
-                    
+                  
 
                     SortedList statusParams = new SortedList();
                     statusParams.Add("@N_CompanyID", nCompanyID);
@@ -101,14 +87,20 @@ namespace SmartxAPI.Controllers
 
 
                 }
-
-                if (ActiveEmployees.Rows.Count > 0) Data.Add("ActiveEmployees", ActiveEmployees);
-                if (ScheduledList.Rows.Count > 0) Data.Add("ScheduledList", ScheduledList);
-                if (ActiveList.Rows.Count > 0) Data.Add("ActiveList", ActiveList);
-                if (CompletedList.Rows.Count > 0) Data.Add("CompletedList", CompletedList);
-                if (ClosedList.Rows.Count > 0) Data.Add("ClosedList", ClosedList);
-                if (ClosedList.Rows.Count > 0) Data.Add("ClosedList", ClosedList);
-                if (TaskStatus.Rows.Count > 0) Data.Add("TaskStatus", TaskStatus);
+                 ActiveEmployees.AcceptChanges();
+                ScheduledList.AcceptChanges();
+                ActiveList.AcceptChanges();
+                CompletedList.AcceptChanges();
+                ClosedList.AcceptChanges();
+                TaskStatus.AcceptChanges();
+                
+                if (ActiveEmployees.Rows.Count > 0) Data.Add("activeEmployees", ActiveEmployees);
+                if (ScheduledList.Rows.Count > 0) Data.Add("scheduledList", ScheduledList);
+                if (ActiveList.Rows.Count > 0) Data.Add("activeList", ActiveList);
+                if (CompletedList.Rows.Count > 0) Data.Add("completedList", CompletedList);
+                if (ClosedList.Rows.Count > 0) Data.Add("closedList", ClosedList);
+               
+                if (TaskStatus.Rows.Count > 0) Data.Add("taskStatus", TaskStatus);
 
                
                 return Ok(api.Success(Data));
@@ -119,8 +111,6 @@ namespace SmartxAPI.Controllers
                 return Ok(api.Error(User, e));
             }
         }
-
-
 
 
 
