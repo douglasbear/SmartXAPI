720414c84b087f1423eb3842164fc16411b50497
aswin <aswin.s@olivotech.com>
2022-03-28 09:16:18 +0530

    scofield

--- Controllers/Tsk_TaskDashBoard.cs
+++ Controllers/Tsk_TaskDashBoard.cs
@@ -30,7 +30,7 @@ namespace SmartxAPI.Controllers
             myFunctions = myFun;
             connectionString = conf.GetConnectionString("SmartxConnection");
         }
-       [HttpGet("details")]
+        [HttpGet("details")]
         public ActionResult GetDashboardDetails(int nFnYearId, int nUserID, int N_Year)
         {
             SortedList Params = new SortedList();
@@ -43,7 +43,7 @@ namespace SmartxAPI.Controllers
             string sqlScheduledList = "select  COUNT(*) as N_ToDoList from  vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_AssigneeID=" + nUserID + " and isnull(B_Closed,0) =0";
             string sqlActiveList = "select Count(*) as N_ActiveList from vw_Tsk_TaskCurrentStatus where N_Status=7 and N_CompanyID=" + nCompanyID + " and  N_CreaterID=" + nUserID + "";
             string sqlCompletedList = "select Count(*) as N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=4";
-            string sqlClosedList = "select Count(*) as N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_CreaterID=" + nUserID + " and N_Status=5";
+            string sqlClosedList = "select Count(*) as N_CompletedList from vw_Tsk_TaskCurrentStatus where N_CompanyID=" + nCompanyID + " and N_ClosedUserID=" + nUserID + " and N_Status=10";
             string sqlTaskStatus = "";
             SortedList Data = new SortedList();
             DataTable ActiveEmployees = new DataTable();
@@ -57,7 +57,7 @@ namespace SmartxAPI.Controllers
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
-              
+
                 {
                     connection.Open();
                     ActiveEmployees = dLayer.ExecuteDataTable(sqlActiveEmployees, Params, connection);
@@ -66,7 +66,7 @@ namespace SmartxAPI.Controllers
                     CompletedList = dLayer.ExecuteDataTable(sqlCompletedList, Params, connection);
                     ClosedList = dLayer.ExecuteDataTable(sqlClosedList, Params, connection);
 
-                  
+
 
                     SortedList statusParams = new SortedList();
                     statusParams.Add("@N_CompanyID", nCompanyID);
@@ -76,32 +76,32 @@ namespace SmartxAPI.Controllers
 
                     try
                     {
-                        TaskStatus=dLayer.ExecuteDataTablePro("Sp_TaskStatus", statusParams, connection);
+                        TaskStatus = dLayer.ExecuteDataTablePro("Sp_TaskStatus", statusParams, connection);
                     }
                     catch (Exception ex)
                     {
-                       
+
                         return Ok(api.Error(User, ex));
                     }
 
 
                 }
-                 ActiveEmployees.AcceptChanges();
+                ActiveEmployees.AcceptChanges();
                 ScheduledList.AcceptChanges();
                 ActiveList.AcceptChanges();
                 CompletedList.AcceptChanges();
                 ClosedList.AcceptChanges();
                 TaskStatus.AcceptChanges();
-                
+
                 if (ActiveEmployees.Rows.Count > 0) Data.Add("ActiveEmployees", ActiveEmployees);
                 if (ScheduledList.Rows.Count > 0) Data.Add("ScheduledList", ScheduledList);
                 if (ActiveList.Rows.Count > 0) Data.Add("ActiveList", ActiveList);
                 if (CompletedList.Rows.Count > 0) Data.Add("CompletedList", CompletedList);
                 if (ClosedList.Rows.Count > 0) Data.Add("ClosedList", ClosedList);
-               
+
                 if (TaskStatus.Rows.Count > 0) Data.Add("TaskStatus", TaskStatus);
 
-               
+
                 return Ok(api.Success(Data));
 
             }
@@ -110,5 +110,49 @@ namespace SmartxAPI.Controllers
                 return Ok(api.Error(User, e));
             }
         }
+        [HttpGet("taskStageList")]
+        public ActionResult GetDashboardStageList()
+
+        {
+
+
+            SortedList Params = new SortedList();
+            DataTable ActiveTasks = new DataTable();
+            int nCompanyID = myFunctions.GetCompanyID(User);
+            string sqlActiveTasks = "SELECT *  FROM vw_Tsk_TaskCurrentStatus WHERE N_CompanyID=" + nCompanyID + " and N_Status=2";//Active Tasks
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+
+                {
+                    connection.Open();
+                    ActiveTasks = dLayer.ExecuteDataTable(sqlActiveTasks, Params, connection);
+                    if (ActiveTasks.Rows.Count > 0)
+                    {
+                        ActiveTasks = api.Format(ActiveTasks, "ActiveTasks");
+                        return Ok(api.Success(ActiveTasks));
+
+                    }
+                    else
+                    {
+                        return Ok(api.Warning("No Results Found"));
+                    }
+
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                return Ok(api.Error(User, e));
+            }
+        }
     }
 }
+
+
+
+
+
+
