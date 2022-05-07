f6744700b7ecec229e1c651510906ebb53117ec0
unknown <akshay.p@olivotech.com>
2022-05-07 12:15:08 +0530

    07-May-Emp List

--- Controllers/Pay_EvaluationSettings.cs
+++ Controllers/Pay_EvaluationSettings.cs
@@ -249,7 +249,7 @@ namespace SmartxAPI.Controllers
             int nCompanyID = myFunctions.GetCompanyID(User);
             Params.Add("@nCompanyID", nCompanyID);
             Params.Add("@nFnYearID", nFnYearID);
-            string sqlCommandText = "Select N_EmpID,X_EmpCode,X_EmpName,X_Department,X_Position from vw_PayEmployee Where N_CompanyID=@nCompanyID and N_Status<2 and N_FnyearID=@nFnYearID";
+            string sqlCommandText = "Select N_EmpID,X_EmpCode,X_EmpName,X_Department,X_Position,N_TemplateID,X_TemplateCode,X_TemplateCode from vw_PayEmployee Where N_CompanyID=@nCompanyID and N_Status<2 and N_FnyearID=@nFnYearID";
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
