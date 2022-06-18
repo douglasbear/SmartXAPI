99c7eda981ddf75f72b7799d82256d9aa685eafc
RatheeshKS <ratheesh.ks@olivotech.com>
2022-06-18 16:15:33 +0530

    Jun 18

--- Controllers/Inv_PurchaseInvoice.cs
+++ Controllers/Inv_PurchaseInvoice.cs
@@ -858,6 +858,20 @@ namespace SmartxAPI.Controllers
                         SortedList ProParam = new SortedList();
                         ProParam.Add("N_CompanyID", nCompanyID);
                         ProParam.Add("N_PKeyID", N_PurchaseID);
+
+                        SortedList ValidationParam = new SortedList();
+                        ValidationParam.Add("N_CompanyID", nCompanyID);
+                        ValidationParam.Add("N_FnYearID", nFnYearID);
+                        ValidationParam.Add("X_Type", "purchase invoice");
+                            try
+                            {
+                                dLayer.ExecuteNonQueryPro("SP_SetupData_Validation", ValidationParam, connection, transaction);
+                            }
+                            catch (Exception ex)
+                            {
+                                transaction.Rollback();
+                                return Ok(_api.Error(User, ex));
+                            }
                          
                         ProParam.Add("X_Type", "purchase invoice");
                         DetailTable = dLayer.ExecuteDataTablePro("SP_ScreenDataImport", ProParam, connection,transaction);
