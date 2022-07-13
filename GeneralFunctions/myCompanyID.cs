cd7b1c918df12dffa9ab6dce1d1a2905a48505c2
RatheeshKS <ratheesh.ks@olivotech.com>
2022-07-13 09:39:24 +0530

    Jul 13

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -117,7 +117,7 @@ namespace SmartxAPI
         public static string _FontName = "Verdana";
         public static string prevVal = "";
         public static string newVal = "";
-        public static string _APIVersion = "22.7.11.1";
+        public static string _APIVersion = "22.7.2.1";
 
 
             }
