09df2374e5bde366e0b2a18d39ef4fb7ecc5ce31
sanjay-olivo <sanjay.kv@olivotech.com>
2022-08-09 11:39:44 -0700

    changes

---
+++ Controllers/Rec_JobVacancy.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using SmartxAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using SmartxAPI.GeneralFunctions;
+using System.Data;
+using System.Collections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace SmartxAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("jobvacancy")]
+    [ApiController]
+    public class Rec_JobVacancy : ControllerBase
+    {
+        private readonly IApiFunctions api;
+        private readonly IDataAccessLayer dLayer;
+        private readonly IMyFunctions myFunctions;
+        private readonly string connectionString;
+
+        private readonly int N_FormID =962 ;
+
+
+        public Rec_JobVacancy(IApiFunctions apifun, IDataAccessLayer dl, IMyFunctions myFun, IConfiguration conf)
+        {
+            api = apifun;
+            dLayer = dl;
+            myFunctions = myFun;
+            connectionString = 
+            conf.GetConnectionString("SmartxConnection");
+        }
+
+
+        [HttpGet("details")]
+        public ActionResult VacancyDetails(int n_VacancyID)
+        {
+            DataSet dt=new DataSet();
+            DataTable MasterTable = new DataTable();
+            SortedList Params = new SortedList();
+            int nCompanyId=myFunctions.GetCompanyID(User);
+            string sqlCommandText = "select * from vw_JobVacancy where N_CompanyID=@p1  and n_VacancyID=@p2";
+            Params.Add("@p1", nCompanyId);  
+            Params.Add("@p2", n_VacancyID);
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MasterTable = dLayer.ExecuteDataTable(sqlCommandText, Params,connection);
+
+                    if (MasterTable.Rows.Count == 0)
+                    {
+                        return Ok(api.Warning("No Results Found"));
+                    }
+                
+                    MasterTable = api.Format(MasterTable, "Master");
+                    dt.Tables.Add(MasterTable);
+                }
+                return Ok(api.Success(dt));               
+            }
+            catch (Exception e)
+            {
+                return Ok(api.Error(User,e));
+            }
+        }
+
+
+
+        //Save....
+        [HttpPost("save")]
+        public ActionResult SaveData([FromBody] DataSet ds)
+        {
+            try
+            {
+                DataTable MasterTable;
+                MasterTable = ds.Tables["master"];
+                int nCompanyID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_CompanyId"].ToString());
+                int nFnYearId = myFunctions.getIntVAL(MasterTable.Rows[0]["n_FnYearId"].ToString());
+                int nVacancyID = myFunctions.getIntVAL(MasterTable.Rows[0]["n_VacancyID"].ToString());
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    SortedList Params = new SortedList();
+                    // Auto Gen
+                    string Code = "";
+                    var values = MasterTable.Rows[0]["X_VacancyCode"].ToString();
+                    if (values == "@Auto")
+                    {
+                        Params.Add("N_CompanyID", nCompanyID);
+                         Params.Add("N_YearID", nFnYearId);
+                        Params.Add("N_FormID", this.N_FormID);
+                        Code = dLayer.GetAutoNumber("Rec_JobVacancy", "X_VacancyCode", Params, connection, transaction);
+                        if (Code == "") { transaction.Rollback();return Ok(api.Error(User,"Unable to generate JobVacancy Code")); }
+                        MasterTable.Rows[0]["X_ClassCode"] = Code;
+                    }
+
+                    if (nVacancyID > 0) 
+                    {  
+                        dLayer.DeleteData("Rec_JobVacancy", "N_VacancyID", nVacancyID, "N_CompanyID =" + nCompanyID, connection, transaction);                        
+                    }
+
+                    string DupCriteria = "N_CompanyID=" + nCompanyID + " and X_VacancyCode='" + Code + "'";
+                    string X_Criteria = "N_CompanyID=" + nCompanyID + "";
+
+                    nVacancyID = dLayer.SaveData("Rec_JobVacancy", "N_VacancyID",DupCriteria,X_Criteria, MasterTable, connection, transaction);
+
+                    if (nVacancyID <= 0)
+                    {
+                        transaction.Rollback();
+                        return Ok(api.Error(User,"Unable to save"));
+                    }
+                    else
+                    {
+                        transaction.Commit();
+                        return Ok(api.Success("Vacancy Created"));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Ok(api.Error(User,ex));
+            }
+        }
+
+               [HttpGet("list")]
+        public ActionResult VacancyList(int nPage, int nSizeperpage, string xSearchkey, string xSortBy)
+        {
+            DataTable dt = new DataTable();
+            SortedList Params = new SortedList();
+            string sqlCommandCount = "";
+            int nCompanyId = myFunctions.GetCompanyID(User);
+            int Count = (nPage - 1) * nSizeperpage;
+            string sqlCommandText = "";
+            string Searchkey = "";
+            if (xSearchkey != null && xSearchkey.Trim() != "")
+                Searchkey = "and (X_VacancyCode like'%" + xSearchkey + "%'or X_PostingTitle like'%" + xSearchkey + "%')";
+
+            if (xSortBy == null || xSortBy.Trim() == "")
+                xSortBy = " order by N_VacancyID desc";
+            else
+                xSortBy = " order by " + xSortBy;
+
+            if (Count == 0)
+                sqlCommandText = "select top(" + nSizeperpage + ") * from vw_JobVacancy where N_CompanyID=@p1 " + Searchkey + " " + xSortBy;
+            else
+                sqlCommandText = "select top(" + nSizeperpage + ") * from vw_JobVacancy where N_CompanyID=@p1 " + Searchkey + " and N_VacancyID not in (select top(" + Count + ") N_VacancyID from vw_JobVacancy where N_CompanyID=@p1 " + xSortBy + " ) " + xSortBy;
+            Params.Add("@p1", nCompanyId);
+
+            SortedList OutPut = new SortedList();
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
+
+                    sqlCommandCount = "select count(*) as N_Count  from vw_JobVacancy where N_CompanyID=@p1";
+                    object TotalCount = dLayer.ExecuteScalar(sqlCommandCount, Params, connection);
+                    OutPut.Add("Details", api.Format(dt));
+                    OutPut.Add("TotalCount", TotalCount);
+                    if (dt.Rows.Count == 0)
+                    {
+                        return Ok(api.Warning("No Results Found"));
+                    }
+                    else
+                    {
+                        return Ok(api.Success(OutPut));
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Ok(api.Error(User, e));
+            }
+        }
+      
+        [HttpDelete("delete")]
+        public ActionResult DeleteData(int nVacancyID)
+        {
+
+            int Results = 0;
+            int nCompanyID=myFunctions.GetCompanyID(User);
+            try
+            {                        
+                SortedList Params = new SortedList();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    Results = dLayer.DeleteData("Rec_JobVacancy", "N_VacancyID", nVacancyID, "N_CompanyID =" + nCompanyID, connection, transaction);
+                
+                    if (Results > 0)
+                    {
+                        transaction.Commit();
+                        return Ok(api.Success("Job Vacancy deleted"));
+                    }
+                    else
+                    {
+                        return Ok(api.Error(User,"Unable to delete Job Vacancy"));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(api.Error(User,ex));
+            }
+
+
+
+        }
+    }
+}
+
