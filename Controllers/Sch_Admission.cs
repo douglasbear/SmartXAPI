e03a213e2593b80ec515e72809240e65a4820c13
Linju-Olivo <linju.ks@olivotech.com>
2022-08-25 12:51:30 +0530

    25-08changes

--- Controllers/Sch_Admission.cs
+++ Controllers/Sch_Admission.cs
@@ -100,7 +100,7 @@ namespace SmartxAPI.Controllers
         }
 
         [HttpGet("details")]
-        public ActionResult AdmissionDetails(string xAdmissionNo, int nAcYearID,int nRegID)
+        public ActionResult AdmissionDetails(string xAdmissionNo, int nAcYearID,int nRegID,int nStudentID)
         {
             DataSet dt=new DataSet();
             DataTable MasterTable = new DataTable();
@@ -110,11 +110,17 @@ namespace SmartxAPI.Controllers
             SortedList Params = new SortedList();
             int nCompanyId=myFunctions.GetCompanyID(User);
             string sqlCommandText = "";
-
-            if (xAdmissionNo!=null)
+           
+            
+            if(nStudentID>0)
+            {
+                Params.Add("@p5", nStudentID);
+                sqlCommandText=" select * from vw_SchAdmission where N_CompanyID=@p1 and N_AdmissionID=@p5 and N_AcYearID=@p4";  
+            }
+            else  if (xAdmissionNo!=null)
             {
                 Params.Add("@p2", xAdmissionNo);
-                sqlCommandText=" select * from vw_SchAdmission where N_CompanyID=@p1 and x_AdmissionNo=@p2 and N_AcYearID=@p4";
+                sqlCommandText=" select * from vw_SchAdmission where N_CompanyID=@p1 and X_AdmissionNo=@p2 and N_AcYearID=@p4";
             }
 
             if(nRegID>0)
@@ -353,9 +359,11 @@ namespace SmartxAPI.Controllers
                             SalesParam.Add("N_BranchID", nBranchID);
                             SalesParam.Add("N_LocationID ", nLocationID);
                             SalesParam.Add("N_StudentID ", nAdmissionID);
-                            SalesParam.Add("N_CustomerID ", nCustomerID);
+                            //SalesParam.Add("N_CustomerID ", nCustomerID);
                             SalesParam.Add("D_AdmDate ", Convert.ToDateTime(MasterTable.Rows[0]["D_AdmissionDate"].ToString()));
                             SalesParam.Add("N_UserID ", nUserID);
+                            SalesParam.Add("N_Type",1);
+                            SalesParam.Add("N_BusRegID",0);
                             try
                             {
                                 dLayer.ExecuteNonQueryPro("SP_StudentAdmFee_Insert", SalesParam, connection, transaction);
