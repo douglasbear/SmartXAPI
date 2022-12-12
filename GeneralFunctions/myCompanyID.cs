f78e1fc3a92621fc5fb283f44128de085ed4d25a
ratheeshks-olivo <ratheeshkdj@gmail.com>
2022-12-12 17:03:43 +0530

    transfer

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -118,7 +118,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-        public static string _APIVersion = "22.12.9.2";
+        public static string _APIVersion = "22.12.9.1";
 
 
     }
