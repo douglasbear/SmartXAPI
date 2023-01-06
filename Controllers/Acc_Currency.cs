9842feb3080f93ffe1f0bee355d874047d2390ad
Linju-Olivo <linju.ks@olivotech.com>
2023-01-06 12:52:25 +0530

    05-12changes

--- Controllers/Acc_Currency.cs
+++ Controllers/Acc_Currency.cs
@@ -228,7 +228,7 @@ namespace SmartxAPI.Controllers
                                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                Results=dLayer.DeleteData("Acc_CurrencyMaster","N_CurrencyID",nCurrencyId,"N_CompanyID="+nCompanyId,connection);
+                Results=dLayer.DeleteData("Acc_CurrencyMaster","N_CurrencyID",nCurrencyId,"",connection);
                 if(Results>0){
                     return Ok(api.Success("Currency deleted" ));
                 }else{
