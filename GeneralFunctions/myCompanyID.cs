55d75c9c6da6bf9e5af91485a5247928c3da8535
arjun-tv <arjun.tv@olivotech.com>
2023-01-14 19:16:31 +0530

    updates

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -118,7 +118,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-        public static string _APIVersion = "23.1.9.2";
+        public static string _APIVersion = "22.12.26.1";
 
     }
 }
