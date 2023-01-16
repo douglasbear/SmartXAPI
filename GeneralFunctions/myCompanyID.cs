da017c874014d7fa4e0a2b74fbd6c5ca149d9e41
sreejesh-olivo <sreejeshkumar@olivotech.com>
2023-01-16 10:30:16 +0530

    16-01-23

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -118,7 +118,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-        public static string _APIVersion = "23.1.13.1";
+        public static string _APIVersion = "22.7.20.1";
 
     }
 }
