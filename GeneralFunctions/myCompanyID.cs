dd1d78ab665de6f7804a9a27a52aa933c0e5c205
Aswin-998 <aswin.s@olivotech.com>
2023-02-27 12:24:36 +0530

    sco

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -117,7 +117,7 @@ namespace SmartxAPI
         public static string _FontName = "Verdana";
         public static string prevVal = "";
         public static string newVal = "";
-        public static string _APIVersion = "23.2.23.2";
+        public static string _APIVersion = "23.2.24.1";
 
     }
 }
