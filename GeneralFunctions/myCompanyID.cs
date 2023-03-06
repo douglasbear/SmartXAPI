96c7337399ca9433183e606f096e668075c9a0c9
RatheeshKS <ratheesh.ks@olivotech.com>
2023-03-06 16:05:32 +0530

    Mar 06

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -118,7 +118,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-        public static string _APIVersion = "23.2.15.1";
+        public static string _APIVersion = "23.3.4.1";
 
     }
 }
