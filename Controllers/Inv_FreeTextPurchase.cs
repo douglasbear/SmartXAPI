62c3cc6b22fcaaedcfc083650bad99ed1e56f413
Aswin-998 <aswin.s@olivotech.com>
2023-03-16 10:03:34 +0530

    sco

--- Controllers/Inv_FreeTextPurchase.cs
+++ Controllers/Inv_FreeTextPurchase.cs
@@ -129,7 +129,7 @@ namespace SmartxAPI.Controllers
                 {
                     connection.Open();
                     object classID = dLayer.ExecuteScalar("select N_ClassID from Inv_itemClass where X_ClassName='Non Stock Item'", Params, connection);
-                    object itemID = dLayer.ExecuteScalar("select N_ItemID From Inv_ItemMaster where N_CompanyID=" + nCompanyID + " and X_ItemCode=001", Params, connection);
+                    object itemID = dLayer.ExecuteScalar("select N_ItemID From Inv_ItemMaster where N_CompanyID=" + nCompanyID + " and X_ItemCode='001'", Params, connection);
                     dt = dLayer.ExecuteDataTable(sqlCommandText, Params, connection);
                     dt = myFunctions.AddNewColumnToDataTable(dt, "N_ItemID", typeof(int), 0);
                     dt = myFunctions.AddNewColumnToDataTable(dt, "N_ClassID", typeof(int), 0);
