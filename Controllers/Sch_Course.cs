8e1b33ed46ff67469e7806973340b0c4d850f653
RatheeshKS <ratheesh.ks@olivotech.com>
2023-03-24 16:37:28 +0530

    Mar 24

--- Controllers/Sch_Course.cs
+++ Controllers/Sch_Course.cs
@@ -205,7 +205,10 @@ namespace SmartxAPI.Controllers
             }
             catch (Exception ex)
             {
-                return Ok(api.Error(User,ex));
+                if (ex.Message.Contains("REFERENCE constraint"))
+                    return Ok(api.Error(User, "Unable to delete course! It has been used."));
+                else
+                    return Ok(api.Error(User, ex));
             }
 
 
