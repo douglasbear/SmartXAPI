6dec3a69180daef9a7666cc27594e116dfd42682
Linju-Olivo <linju.ks@olivotech.com>
2023-05-09 10:46:48 +0530

    msges

--- GeneralFunctions/myCompanyID.cs
+++ GeneralFunctions/myCompanyID.cs
@@ -83,7 +83,7 @@ namespace SmartxAPI
         public static string prevVal = "";
         public static string newVal = "";
 
-        public static string _APIVersion = "23.5.2.1";
+        public static string _APIVersion = "23.4.29.1";
 
     }
 }
