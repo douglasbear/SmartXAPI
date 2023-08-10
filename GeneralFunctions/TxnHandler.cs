f7df539cca22658778f5e25a3d9e3e15084569d7
Aswin-998 <aswin.s@olivotech.com>
2023-08-10 16:05:07 +0530

    sco

--- GeneralFunctions/TxnHandler.cs
+++ GeneralFunctions/TxnHandler.cs
@@ -1100,7 +1100,15 @@ namespace SmartxAPI.GeneralFunctions
                         Params.Add("N_FormID", 1346);
                     else
                         Params.Add("N_FormID", N_FormID);
-                    Params.Add("N_BranchID", MasterRow["n_BranchId"].ToString());
+                    if(myFunctions.getIntVAL(MasterRow["N_DivisionID"].ToString())>0)
+                    {
+                         //Params.Add("N_BranchID", MasterRow["n_BranchId"].ToString());
+                    }
+                    else
+                    {
+                        Params.Add("N_BranchID", MasterRow["n_BranchId"].ToString());
+                    }
+                    
                     while (true)
                     {
                         InvoiceNo = dLayer.ExecuteScalarPro("SP_AutoNumberGenerate", Params, connection, transaction).ToString();
