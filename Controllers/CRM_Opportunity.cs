561fe6774f11aecc29f82403fd453a034e4f459e
Aswin-998 <aswin.s@olivotech.com>
2023-10-14 18:18:25 +0530

    sco

--- Controllers/CRM_Opportunity.cs
+++ Controllers/CRM_Opportunity.cs
@@ -227,9 +227,23 @@ namespace SmartxAPI.Controllers
                 int nBranchId = myFunctions.getIntVAL(MasterTable.Rows[0]["n_BranchId"].ToString());
                 int nWActivityID = myFunctions.getIntVAL(MasterTable.Rows[0]["N_WActivityID"].ToString());
                 int nTaskOwner = myFunctions.getIntVAL(MasterTable.Rows[0]["N_SalesmanID"].ToString());
-                string X_ContactEmail = MasterTable.Rows[0]["X_Email"].ToString();
-                string X_ContactNumber = MasterTable.Rows[0]["X_Mobile"].ToString();
-                int nContactID = myFunctions.getIntVAL(MasterTable.Rows[0]["N_ContactID"].ToString());
+                string X_ContactEmail ="";
+                string X_ContactNumber ="";
+                int nContactID =0;
+                 if(MasterTable.Columns.Contains("X_Email"))
+                 {
+                    X_ContactEmail= MasterTable.Rows[0]["X_Email"].ToString();
+                 }
+                 if(MasterTable.Columns.Contains("X_Mobile"))
+                 {
+                    X_ContactNumber= MasterTable.Rows[0]["X_Mobile"].ToString();
+                 }
+                 if(MasterTable.Columns.Contains("N_ContactID"))
+                 {
+                    nContactID= myFunctions.getIntVAL(MasterTable.Rows[0]["N_ContactID"].ToString());
+                 }
+
+
                 object N_WorkFlowID = "";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
