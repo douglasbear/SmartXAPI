c700b05d9be6bfe62866e2cc21d4a2d6eacd944b
RatheeshKS <ratheesh.ks@olivotech.com>
2023-11-21 13:39:09 +0530

    Nov 21

--- Controllers/Inv_SalesInvoice.cs
+++ Controllers/Inv_SalesInvoice.cs
@@ -842,7 +842,7 @@ namespace SmartxAPI.Controllers
                         if (enableDayWise)
                             DetailSql = "select * from vw_ServiceTimesheetDetailsToSalesDaywise where N_CompanyId=@nCompanyID and N_ServiceSheetID in ( " + xServiceSheetID + " )";
                         else
-                            DetailSql = "select * from vw_ServiceTimesheetDetailsToSales where N_CompanyId=@nCompanyID and N_ServiceSheetID in ( " + xServiceSheetID + " )";
+                            DetailSql = "select * from vw_ServiceTimesheetDetailsToSales where N_CompanyId=@nCompanyID and N_ServiceSheetID in ( " + xServiceSheetID + " ) order by N_SalesOrderDetailsID desc";
 
                         DataTable DetailTable = dLayer.ExecuteDataTable(DetailSql, QueryParamsList, Con);
                         DetailTable = _api.Format(DetailTable, "Details");
