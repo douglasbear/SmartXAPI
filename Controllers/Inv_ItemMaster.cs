36effc684fd39e8ba4931e352ffe47d7d319710c
Linju-Olivo <linju.ks@olivotech.com>
2024-01-20 14:40:37 +0530

    20-01

--- Controllers/Inv_ItemMaster.cs
+++ Controllers/Inv_ItemMaster.cs
@@ -118,7 +118,7 @@ namespace SmartxAPI.Controllers
                 Condition = Condition + " and vw_InvItem_Search_cloud.N_ItemID<> " + nNotGridItemID;
 
             if (nLocationID != 0)
-                Condition = Condition + "  and vw_InvItem_Search_cloud.N_ItemID in (Select N_ItemID from Inv_ItemMasterWHLink where N_CompanyID=@p1 and N_WarehouseID=" + nLocationID + " )  ";
+                Condition = Condition + "  and vw_InvItem_Search_cloud.N_ItemID in (Select N_ItemID from Inv_ItemMasterWHLink where N_CompanyID=@p1 and N_WarehouseID=" + nLocationID + " ) and N_WarehouseID=" + nLocationID + "";
             if (isStockItem)
                 Condition = Condition + " and N_ClassID =2";
             if (isServiceItem)
